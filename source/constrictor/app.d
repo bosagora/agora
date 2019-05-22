a1d994487e7babf5141e92067b8130db16a2a1de
Geod24 <pro.mathias.lang@gmail.com>
2019-05-22 12:04:57 +0900

    Add skeleton for app / dub / travis

---
+++ source/constrictor/app.d
@@ -0,0 +1,23 @@
+/*******************************************************************************
+
+    Entry point for Constrictor node
+
+    Copyright:
+        Copyright (c) 2019 BOS Platform Foundation Korea
+        All rights reserved.
+
+    License:
+        MIT License. See LICENSE for details.
+
+*******************************************************************************/
+
+module constrictor.app;
+
+void main()
+{
+	// TODO:
+    // - Config parsing
+    // - Network interface setup
+    // - Event loop
+    // - End world hunger
+}
