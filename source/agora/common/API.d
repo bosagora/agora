d2c03c8de8934bb017e52853a54ed5391e59538f
Andrej Mitrovic <andrej.mitrovich@gmail.com>
2019-06-14 14:59:35 +0900

    Linker error test-case

---
+++ source/agora/common/API.d
@@ -0,0 +1,20 @@
+/*******************************************************************************
+
+    Contains the REST API interface.
+
+    Copyright:
+        Copyright (c) 2019 BOS Platform Foundation Korea
+        All rights reserved.
+
+    License:
+        MIT License. See LICENSE for details.
+
+
+*******************************************************************************/
+
+module agora.common.API;
+
+import scpd.types.Stellar_SCP;
+
+import vibe.web.rest;
+
