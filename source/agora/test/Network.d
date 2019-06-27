8ffc1b54e0f647025b55badf8cb2048c6acfefbb
Geod24 <pro.mathias.lang@gmail.com>
2019-06-27 20:50:22 +0900

    Ensure that spawned nodes know of each other

--- source/agora/test/Network.d
+++ source/agora/test/Network.d
@@ -20,8 +20,19 @@ import agora.test.Base;
 ///
 unittest
 {
+    import std.algorithm;
+    import std.format;
+
     const NodeCount = 4;
     auto network = makeTestNetwork!TestNetworkManager(NetworkTopology.Simple, NodeCount);
     network.start();
     assert(network.getDiscoveredNodes().length == NodeCount);
+    // Check that each node knows of the others
+    assert(network.apis.byKey().count == NodeCount);
+    foreach (key, node; network.apis)
+    {
+        auto addresses = node.getNetworkInfo().addresses.keys;
+        assert(addresses.sort.uniq.count == NodeCount - 1,
+               format("Node %s has %d peers: %s", key, addresses.length, addresses));
+    }
 }
