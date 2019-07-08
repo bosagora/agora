f579d1e2b95a4bc5508c85f338179439ece94d45
Andrej Mitrovic <andrej.mitrovich@gmail.com>
2019-07-08 10:54:59 +0900

    Add hashing support to BlockHeader

--- source/agora/common/Block.d
+++ source/agora/common/Block.d
@@ -16,6 +16,7 @@
 module agora.common.Block;
 
 import agora.common.Data;
+import agora.common.Hash;
 import agora.common.Transaction;
 
 
@@ -37,6 +38,45 @@ public struct BlockHeader
     /// The hash of the only transaction in the block
     /// (later to be replaced with a merkle root)
     public Hash tx_hash;
+
+
+    /***************************************************************************
+
+        Implements hashing support
+
+        Params:
+            dg = Hashing function accumulator
+
+    ***************************************************************************/
+
+    public void computeHash (scope HashDg dg) const nothrow @safe @nogc
+    {
+        dg(this.prev_block[]);
+        hashPart(this.height, dg);
+        dg(this.tx_hash[]);
+    }
+}
+
+/// hashing test
+unittest
+{
+    import agora.common.crypto.Key;
+    auto address = `GDD5RFGBIUAFCOXQA246BOUPHCK7ZL2NSHDU7DVAPNPTJJKVPJMNLQFW`;
+    PublicKey pubkey = PublicKey.fromString(address);
+
+    // above parts not @safe/@nogc yet
+    () @safe @nogc nothrow
+    {
+        Output[1] outputs = [ Output(100, pubkey) ];
+        Transaction tx = { outputs: outputs[] };
+        BlockHeader header = { tx_hash : tx.hashFull() };
+
+        auto hash = hashFull(header);
+        auto exp_hash = Hash("0xc45f765d063deece0348f9e7d47287fec5324395b1c" ~
+            "065cde457dbb3c809cbb680450e86ac86bac50ce0d5cd8b1f9b04b29c1b2e5" ~
+            "4cee5d6d5aaac38bb4dca3c");
+        assert(hash == exp_hash);
+    }();
 }
 
 /*******************************************************************************
