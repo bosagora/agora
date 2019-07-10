3ba0a22013f5ebfe0653e3022d95a272866b1e3a
Geod24 <pro.mathias.lang@gmail.com>
2019-07-10 13:39:39 +0900

    Add a module to perform Schnorr signatures
    Currently not used in production but allow us to experiement
    with signature aggregation in the consensus protocol.

--- ci/travis_linux_setup.sh
+++ ci/travis_linux_setup.sh
@@ -6,3 +6,13 @@ set -o pipefail
 mkdir -p $HOME/bin/
 ln -s `which gcc-9` $HOME/bin/gcc # /usr/bin/gcc-9
 ln -s `which g++-9` $HOME/bin/g++ # /usr/bin/g++-9
+
+pushd $HOME
+wget https://github.com/jedisct1/libsodium/archive/1.0.18-RELEASE.tar.gz
+tar xvfz 1.0.18-RELEASE.tar.gz
+cd libsodium-1.0.18-RELEASE
+./configure
+make -j4
+sudo make install
+sudo ldconfig # Refresh cache
+popd
