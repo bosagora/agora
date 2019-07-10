62b75b391cc89c029bd0f50f30b547a05f73c2df
Geod24 <pro.mathias.lang@gmail.com>
2019-07-10 13:39:39 +0900

    Add a module to perform Schnorr signatures
    Currently not used in production but allow us to experiement
    with signature aggregation in the consensus protocol.

---
+++ source/agora/common/crypto/ECC.d
@@ -0,0 +1,211 @@
+/*******************************************************************************
+
+    Elliptic-curve primitives
+
+    Those primitives are used for Schnorr signatures.
+
+    See_Also:
+      https://en.wikipedia.org/wiki/EdDSA#Ed25519
+
+    Copyright:
+        Copyright (c) 2019 BOS Platform Foundation Korea
+        All rights reserved.
+
+    License:
+        MIT License. See LICENSE for details.
+
+*******************************************************************************/
+
+module agora.common.crypto.ECC;
+
+import agora.common.Hash;
+import geod24.bitblob;
+import libsodium;
+
+///
+nothrow @nogc unittest
+{
+    const Scalar s1 = Scalar.random();
+    const Scalar s2 = Scalar.random();
+    const Scalar s3 = s1 + s2;
+
+    assert(s3 - s1 == s2);
+    assert(s3 - s2 == s1);
+    assert(s3 - s3 == Scalar.init);
+    assert(-s3 == -s1 - s2);
+    assert(-s3 == -s2 - s1);
+
+    const Scalar One = (s3 + (-s3));
+    assert(One * One == One);
+    // Get the generator
+    const Point G = One.toPoint();
+    assert(G + G == (One + One).toPoint());
+
+    const Point p1 = s1.toPoint();
+    const Point p2 = s2.toPoint();
+    const Point p3 = s3.toPoint();
+
+    assert(s1.toPoint() == p1);
+    assert(p3 - p1 == p2);
+    assert(p3 - p2 == p1);
+
+    assert(s1 * p2 + s2 * p2 == s3 * p2);
+}
+
+/*******************************************************************************
+
+    A field element in the finite field of order 2^255-19
+
+    Scalar are used as private key and source of noise for signatures.
+
+*******************************************************************************/
+
+public struct Scalar
+{
+    /// Internal state
+    package BitBlob!(crypto_core_ed25519_SCALARBYTES * 8) data;
+
+    /// Construct a scalar from its string representation or a `ubyte[]`
+    public this (T) (T param)
+        if (is(typeof(this.data = typeof(this.data)(param))))
+    {
+        this.data = typeof(this.data)(param);
+    }
+
+    /// Reduce the hash to a scalar
+    public this (Hash param) nothrow @nogc
+    {
+        crypto_core_ed25519_scalar_reduce(this.data[].ptr, param[].ptr);
+    }
+
+    /// Expose `toString`
+    public void toString (scope void delegate(const(char)[]) @safe dg)
+        const @safe
+    {
+        this.data.toString(dg);
+    }
+
+    /// Operator overloads for `+`, `-`, `*`
+    public Scalar opBinary (string op)(const scope auto ref Scalar rhs)
+        const nothrow @nogc
+    {
+        Scalar result = void;
+        static if (op == "+")
+            crypto_core_ed25519_scalar_add(
+                result.data[].ptr, this.data[].ptr, rhs.data[].ptr);
+        else static if (op == "-")
+            crypto_core_ed25519_scalar_sub(
+                result.data[].ptr, this.data[].ptr, rhs.data[].ptr);
+        else static if (op == "*")
+            crypto_core_ed25519_scalar_mul(
+                result.data[].ptr, this.data[].ptr, rhs.data[].ptr);
+        else
+            static assert(0, "Operator " ~ op ~ " not implemented");
+        return result;
+    }
+
+    /// Get the complement of this scalar
+    public Scalar opUnary (string s)()
+        const nothrow @nogc
+        if (s == "-")
+    {
+        Scalar result = void;
+        crypto_core_ed25519_scalar_complement(result.data[].ptr, this.data[].ptr);
+        return result;
+    }
+
+    /// Generate a random scalar
+    public static Scalar random () nothrow @nogc
+    {
+        Scalar ret = void;
+        crypto_core_ed25519_scalar_random(ret.data[].ptr);
+        return ret;
+    }
+
+    /// Return the point corresponding to this scalar multiplied by the generator
+    public Point toPoint () const nothrow @nogc
+    out (val) { assert(crypto_core_ed25519_is_valid_point(val.data[].ptr)); }
+    do {
+        Point ret = void;
+        if (crypto_scalarmult_ed25519_base_noclamp(ret.data[].ptr, this.data[].ptr))
+            assert(0);
+        return ret;
+    }
+}
+
+/*******************************************************************************
+
+    Represent a point on Curve25519
+
+    A point is an element of the cyclic subgroup formed from the elliptic curve:
+    x^2 + y^2 = 1 - (121665 / 1216666) * x^2 * y^2
+    And the base point `B` where By=4/5 and Bx > 0.
+
+*******************************************************************************/
+
+public struct Point
+{
+    /// Internal state
+    package BitBlob!(crypto_core_ed25519_BYTES * 8) data;
+
+    /// Construct a point from its string representation or a `ubyte[]`
+    public this (T) (T param)
+        nothrow @nogc
+        if (is(typeof(this.data = typeof(this.data)(param))))
+    {
+        this.data = typeof(this.data)(param);
+    }
+
+    /// Expose `toString`
+    public void toString (scope void delegate(const(char)[]) @safe dg)
+        const @safe
+    {
+        this.data.toString(dg);
+    }
+
+    /// Operator overloads for points additions
+    public Point opBinary (string op)(const scope auto ref Point rhs)
+        const nothrow @nogc
+        if (op == "+" || op == "-")
+    {
+        Point result = void;
+        static if (op == "+")
+        {
+            if (crypto_core_ed25519_add(
+                    result.data[].ptr, this.data[].ptr, rhs.data[].ptr))
+                assert(0);
+        }
+        else static if (op == "-")
+        {
+            if (crypto_core_ed25519_sub(
+                    result.data[].ptr, this.data[].ptr, rhs.data[].ptr))
+                assert(0);
+        }
+        else static assert(0, "Unhandled `" ~ op ~ "` operator for Point");
+        return result;
+    }
+
+    /// Operator overloads for scalar multiplication
+    public Point opBinary (string op)(const scope auto ref Scalar rhs)
+        const nothrow @nogc
+        if (op == "*")
+    {
+        Point result = void;
+        if (crypto_scalarmult_ed25519_noclamp(
+                result.data[].ptr, rhs.data[].ptr, this.data[].ptr))
+            assert(0);
+        return result;
+    }
+
+    /// Ditto
+    public Point opBinaryRight (string op)(const scope auto ref Scalar lhs)
+        const nothrow @nogc
+        if (op == "*")
+    {
+        Point result = void;
+        if (crypto_scalarmult_ed25519_noclamp(
+                result.data[].ptr, lhs.data[].ptr, this.data[].ptr))
+            assert(0);
+        return result;
+    }
+}
---
+++ source/agora/common/crypto/Schnorr.d
@@ -0,0 +1,239 @@
+/*******************************************************************************
+
+    Low level utilities to perform Schnorr signatures on Curve25519.
+
+    Through this module, lowercase letters represent scalars and uppercase
+    letters represent points. Multiplication of a scalar by a point,
+    which is adding a point to itself multiple times, is represented with '*',
+    e.g. `a * G`. Uppercase letters are point representations of scalars,
+    that is, the scalar multipled by the generator, e.g. `r == r * G`.
+    `x` is the private key, `X` is the public key, and `H()` is the Blake2b
+    512 bits hash reduced to a scalar in the field.
+
+    Following the Schnorr BIP (see links), signatures are of the form
+    `(R,s)` and satisfy `s * G = R + H(X || R || m) * X`.
+    `r` is refered to as the nonce and is a cryptographically randomly
+    generated number that should neither be reused nor leaked.
+
+    Signature_Aggregation:
+    Since Schnorr signatures use a linear equation, they can be simply
+    combined with addition, enabling `O(1)` signature verification
+    time and `O(1)` and `O(1)` signature size.
+    Additionally, since the `c` factor does not depend on EC operation,
+    we can do batch verification, enabling us to speed up verification
+    when verifying large amount of data (e.g. a block).
+
+    See_Also:
+      - https://en.wikipedia.org/wiki/Curve25519
+      - https://en.wikipedia.org/wiki/Schnorr_signature
+      - https://github.com/sipa/bips/blob/bip-schnorr/bip-schnorr.mediawiki
+      - https://www.secg.org/sec1-v2.pdf
+
+    TODO:
+      - Compress signature according to SEC1 v2 (Section 2.3) (#304)
+      - Audit GDC and LDC generated code
+      - Proper audit
+
+    Copyright:
+        Copyright (c) 2019 BOS Platform Foundation Korea
+        All rights reserved.
+
+    License:
+        MIT License. See LICENSE for details.
+
+*******************************************************************************/
+
+module agora.common.crypto.Schnorr;
+
+import agora.common.Data;
+import agora.common.Hash;
+import agora.common.crypto.ECC;
+
+import std.algorithm;
+import std.range;
+
+
+/// Single signature example
+nothrow @nogc unittest
+{
+    Pair kp = Pair.random();
+    auto signature = sign(kp, "Hello world");
+    assert(verify(kp.V, signature, "Hello world"));
+}
+
+/// Multi-signature example
+nothrow @nogc unittest
+{
+    // Setup
+    static immutable string secret = "BOSAGORA for the win";
+    Pair kp1 = Pair.random();
+    Pair kp2 = Pair.random();
+    Pair R1 = Pair.random();
+    Pair R2 = Pair.random();
+    Point R = R1.V + R2.V;
+    Point X = kp1.V + kp2.V;
+
+    auto sig1 = sign(kp1.v, X, R, R1.v, secret);
+    auto sig2 = sign(kp2.v, X, R, R2.v, secret);
+    auto sig3 = Signature(R, sig1.s + sig2.s);
+
+    // No one can verify any of those individually
+    assert(!verify(kp1.V, sig1, secret));
+    assert(!verify(kp1.V, sig2, secret));
+    assert(!verify(kp2.V, sig2, secret));
+    assert(!verify(kp2.V, sig1, secret));
+    assert(!verify(kp1.V, sig3, secret));
+    assert(!verify(kp2.V, sig3, secret));
+
+    // But multisig works
+    assert(verify(X, sig3, secret));
+}
+
+/// Represent a signature (R, s)
+public struct Signature
+{
+    /// Commitment
+    public Point R;
+    /// Proof
+    public Scalar s;
+}
+
+/// Represent the message to hash (part of `c`)
+private struct Message (T)
+{
+    public Point X;
+    public Point R;
+    public T     message;
+
+    ///
+    public void computeHash(scope HashDg dg) const nothrow @nogc
+    {
+        dg(this.X.data[]);
+        dg(this.R.data[]);
+        static if (is(T : const(ubyte)[]))
+            dg(this.message);
+        else
+            hashPart(this.message, dg);
+    }
+}
+
+
+/// Contains a scalar and its projection on the elliptic curve (`v` and `v.G`)
+public struct Pair
+{
+    /// A PRNGenerated number
+    public Scalar v;
+    /// v.G
+    public Point V;
+
+    /// Generate a random value `v` and a point on the curve `V` where `V = v.G`
+    public static Pair random () nothrow @nogc
+    {
+        Scalar sc = Scalar.random();
+        return Pair(sc, sc.toPoint());
+    }
+}
+
+/// Single-signer trivial API
+private Signature sign (T) (const ref Pair kp, auto ref T data)
+    nothrow @nogc
+{
+    const R = Pair.random();
+    return sign!T(kp.v, kp.V, R.V, R.v, data);
+}
+
+/// Single-signer privkey API
+private Signature sign (T) (const ref Scalar privateKey, T data)
+    nothrow @nogc
+{
+    const R = Pair.random();
+    return sign!T(privateKey, privateKey.toPoint(), R.V, R.v, data);
+}
+
+/// Complex API, allow multisig
+private Signature sign (T) (
+    const ref Scalar x, const ref Point X,
+    const ref Point R, const ref Scalar r,
+    auto ref T data)
+    nothrow @nogc
+{
+    /*
+      G := Generator point:
+      15112221349535400772501151409588531511454012693041857206046113283949847762202,
+      46316835694926478169428394003475163141307993866256225615783033603165251855960
+      x := private key
+      X := public key (x.G)
+      r := random number
+      R := commitment (r.G)
+      c := Hash(X || R || message)
+
+      Proof = (R, s)
+      Signature/Verify: R + c*X == s.G
+      Multisig:
+      R = (r0 + r1 + rn).G == (R0 + R1 + Rn)
+      X = (X0 + X1 + Xn)
+      To get `c`, need to precommit `R`
+     */
+    // Compute the challenge and reduce the hash to a scalar
+    Scalar c = hashFull(Message!T(X, R, data));
+    // Compute `s` part of the proof
+    Scalar s = r + (c * x);
+    return Signature(R, s);
+}
+
+/*******************************************************************************
+
+    Verify that a signature matches the provided data
+
+    Params:
+      T = Type of data being signed
+      X = The point corresponding to the public key
+      s = Signature to verify
+      data = Data to sign (the hash will be signed)
+
+    Returns:
+      Whether or not the signature is valid for (X, s, data).
+
+*******************************************************************************/
+
+public bool verify (T) (const ref Point X, const ref Signature s, auto ref T data)
+    nothrow @nogc
+{
+    // Compute the challenge and reduce the hash to a scalar
+    Scalar c = hashFull(Message!T(X, s.R, data));
+    // Compute `R + c*X`
+    Point RcX = s.R + (c * X);
+    /// Compute `s.G`
+    auto S = s.s.toPoint();
+    return S == RcX;
+}
+
+///
+nothrow @nogc unittest
+{
+    Scalar key = Scalar(`0x074360d5eab8e888df07d862c4fc845ebd10b6a6c530919d66221219bba50216`);
+    Pair kp = Pair(key, key.toPoint());
+    auto signature = sign(kp, "Hello world");
+    assert(verify(kp.V, signature, "Hello world"));
+}
+
+nothrow @nogc unittest
+{
+    Scalar key = Scalar(`0x074360d5eab8e888df07d862c4fc845ebd10b6a6c530919d66221219bba50216`);
+    Pair kp = Pair(key, key.toPoint());
+    auto signature = sign(kp, "Hello world.");
+    assert(!verify(kp.V, signature, "Hello world"));
+}
+
+nothrow @nogc unittest
+{
+    static immutable string secret = "BOSAGORA for the win";
+    Pair kp1 = Pair.random();
+    Pair kp2 = Pair.random();
+    auto sig1 = sign(kp1, secret);
+    auto sig2 = sign(kp2, secret);
+    assert(verify(kp1.V, sig1, secret));
+    assert(!verify(kp1.V, sig2, secret));
+    assert(verify(kp2.V, sig2, secret));
+    assert(!verify(kp2.V, sig1, secret));
+}
