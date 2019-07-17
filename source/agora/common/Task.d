54569db1e12201638e4da994ccbb13a3bf1837f9
Andrej Mitrovic <andrej.mitrovich@gmail.com>
2019-07-17 10:48:10 +0900

    Implement the TaskManager class
    The TaskManager allows spawning new
    tasks that are by default backed
    by vibe.d's event loop.
    
    Inherited classes may override this
    behavior to spawn tasks which run
    in their own defined event loops.
    
    This simplifies code by not requiring
    an instance of the Networking class
    in order to manage new / existing tasks.
    
    The TaskManager class is subclassed in the
    unittests in order to use LocalRest's
    event loop, and avoid I/O in the process.

---
+++ source/agora/common/Task.d
@@ -0,0 +1,54 @@
+/*******************************************************************************
+
+    Contains a task manager backed by vibe.d's event loop.
+
+    Overriding classes can implement task routines to run
+    tasks in their own event loop - for example to be used
+    with LocalRest to simulate a network and avoid any I/O.
+
+    Copyright:
+        Copyright (c) 2019 BOS Platform Foundation Korea
+        All rights reserved.
+
+    License:
+        MIT License. See LICENSE for details.
+
+*******************************************************************************/
+
+module agora.common.Task;
+
+import core.time;
+
+/// Ditto
+public class TaskManager
+{
+    /***************************************************************************
+
+        Run an asynchronous task in vibe.d's event loop
+
+        Params:
+            dg = the delegate the task should run
+
+    ***************************************************************************/
+
+    public void runTask ( void delegate() dg)
+    {
+        static import vibe.core.core;
+        vibe.core.core.runTask(dg);
+    }
+
+    /***************************************************************************
+
+        Suspend the current task for the given duration
+
+        Params:
+            dur = the duration for which to suspend the task for
+
+    ***************************************************************************/
+
+    public void wait (Duration dur)
+    {
+        static import vibe.core.core;
+        vibe.core.core.sleep(dur);
+    }
+}
