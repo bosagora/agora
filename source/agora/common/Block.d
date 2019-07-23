a1c861e13e5fd5dfafcb77fdf683872288740110
MukeunKim <mukeun.kim@bpfkorea.org>
2019-07-23 17:40:54 +0900

    Implemented multi-transaction blocks

--- source/agora/common/Block.d
+++ source/agora/common/Block.d
@@ -21,6 +21,8 @@ import agora.common.Hash;
 import agora.common.Transaction;
 import agora.node.BlockSerialize;
 
+import std.algorithm.comparison;
+
 /*******************************************************************************
 
     The block header which contains a link to the previous block header,
@@ -110,7 +112,11 @@ public struct Block
     public BlockHeader header;
 
     ///
-    public Transaction tx;
+    public Transaction[] txs;
+
+    ///
+    public Hash[] merkle_tree;
+
 
     /***************************************************************************
 
@@ -123,8 +129,93 @@ public struct Block
 
     public void serialize (scope SerializeDg dg) pure const nothrow @safe
     {
-        serializePart(header, dg);
-        serializePart(tx, dg);
+        serializePart(this.header, dg);
+        foreach (ref tx; this.txs)
+            serializePart(tx, dg);
+    }
+
+    /***************************************************************************
+
+        Build merkle tree
+
+        Returns:
+            Return merkle root
+
+    ***************************************************************************/
+
+    public Hash buildMerkleTree() @safe
+    {
+        this.merkle_tree.length = this.txs.length;
+        foreach (size_t idx, ref hash; this.merkle_tree)
+            hash = hashFull(this.txs[idx]);
+
+        size_t j = 0;
+        for (size_t length = this.txs.length; length > 1; length = (length + 1) / 2)
+        {
+            for (size_t i = 0; i < length; i += 2)
+            {
+                size_t i2 = min(i + 1, length - 1);
+                this.merkle_tree ~= mergeHash(this.merkle_tree[j + i], this.merkle_tree[j + i2]);
+            }
+            j += length;
+        }
+        return ((this.merkle_tree.length == 0) ? Hash.init : this.merkle_tree[$ - 1]);
+    }
+
+    /*******************************************************************************
+
+        Get merkle branch
+
+        Params:
+            index = Sequence of transactions
+
+        Returns:
+            Return merkle branch
+
+    *******************************************************************************/
+
+    public Hash[] getMerkleBranch(size_t index) @safe
+    {
+        if (this.merkle_tree.length == 0)
+            this.buildMerkleTree();
+
+        Hash[] merkle_branch;
+        size_t j = 0;
+        for (size_t length = this.txs.length; length > 1; length = (length + 1) / 2)
+        {
+            size_t i = min(index ^ 1, length - 1);
+            merkle_branch ~= this.merkle_tree[j + i];
+            index >>= 1;
+            j += length;
+        }
+        return merkle_branch;
+    }
+
+    /*******************************************************************************
+
+        Calculate the merkle root using the merkle branch.
+
+        Params:
+            hash = `Hash` of `Transaction`
+            merkle_branch = `Hash` of merkle branch
+            index = Index of the hash in the array of transactions. It starts at zero.
+
+        Returns:
+            Return `Hash` of merkle root.
+
+    *******************************************************************************/
+
+    public static Hash checkMerkleBranch(Hash hash, const ref Hash[] merkle_branch, size_t index) @safe
+    {
+        foreach(const ref otherside; merkle_branch)
+        {
+            if (index & 1)
+                hash = mergeHash(otherside, hash);
+            else
+                hash = mergeHash(hash, otherside);
+            index >>= 1;
+        }
+        return hash;
     }
 }
 
@@ -163,9 +254,9 @@ public Block getGenesisBlock ()
     auto gen_tx = newCoinbaseTX(getGenesisKeyPair().address, 40_000_000);
     auto header = BlockHeader(
         Hash.init, 0,
-        hashFull(gen_tx));
+        mergeHash(hashFull(gen_tx),hashFull(gen_tx)));
 
-    return Block(header, gen_tx);
+    return Block(header, [gen_tx]);
 }
 
 ///
@@ -182,18 +273,19 @@ unittest
 
     Params:
         prev_block = the previous block
-        tx = the transaction that will be contained in the new block
+        txs = the transactions that will be contained in the new block
 
 *******************************************************************************/
 
-public Block makeNewBlock (Block prev_block, Transaction tx) @nogc nothrow @safe
+public Block makeNewBlock (Block prev_block, Transaction[] txs) @safe
 {
     Block block;
 
     block.header.prev_block = prev_block.header.hashFull();
     block.header.height = prev_block.header.height + 1;
-    block.header.tx_hash = tx.hashFull();
-    block.tx = tx;
+    block.txs ~= txs;
+
+    block.header.tx_hash = block.buildMerkleTree();
 
     return block;
 }
@@ -204,9 +296,83 @@ unittest
     Block gen_block = getGenesisBlock();
 
     // above parts not @safe/@nogc yet
-    () @safe @nogc nothrow
+    () @safe
     {
-        auto new_block = makeNewBlock(gen_block, Transaction.init);
+        auto new_block = makeNewBlock(gen_block, [Transaction.init]);
         assert(new_block.header.prev_block == hashFull(gen_block.header));
     }();
+}
+
+
+/// Test of Merkle Path and Merkle Proof
+unittest
+{
+    Transaction[] txs;
+    Hash[] merkle_branch;
+
+    KeyPair[] key_pairs = [
+        KeyPair.random(),
+        KeyPair.random(),
+        KeyPair.random(),
+        KeyPair.random(),
+        KeyPair.random(),
+        KeyPair.random(),
+        KeyPair.random(),
+        KeyPair.random(),
+        KeyPair.random()
+    ];
+
+    // Create transactions.
+    Transaction tx;
+    Hash last_hash = Hash.init;
+    for (int idx = 0; idx < 8; idx++)
+    {
+        tx = Transaction([Input(last_hash, 0)],[Output(100_000, key_pairs[idx+1].address)]);
+        last_hash = hashFull(tx);
+        tx.inputs[0].signature = key_pairs[idx].secret.sign(last_hash[]);
+        txs ~= tx;
+    }
+
+    Block block;
+
+    block.header.prev_block = Hash.init;
+    block.header.height = 0;
+    block.txs ~= txs;
+    block.header.tx_hash = block.buildMerkleTree();
+
+    const Hash h0 = hashFull(txs[0]);
+    const Hash h1 = hashFull(txs[1]);
+    const Hash h2 = hashFull(txs[2]);
+    const Hash h3 = hashFull(txs[3]);
+    const Hash h4 = hashFull(txs[4]);
+    const Hash h5 = hashFull(txs[5]);
+    const Hash h6 = hashFull(txs[6]);
+    const Hash h7 = hashFull(txs[7]);
+
+    const Hash h01 = mergeHash(h0, h1);
+    const Hash h23 = mergeHash(h2, h3);
+    const Hash h45 = mergeHash(h4, h5);
+    const Hash h67 = mergeHash(h6, h7);
+
+    const Hash h0123 = mergeHash(h01, h23);
+    const Hash h4567 = mergeHash(h45, h67);
+
+    const Hash h01234567 = mergeHash(h0123, h4567);
+
+    assert(block.header.tx_hash == h01234567, "Error in MerkleTree.");
+
+    // Merkle Proof
+    merkle_branch = block.getMerkleBranch(2);
+    assert(merkle_branch.length == 3);
+    assert(merkle_branch[0] == h3, "Error in the merkle path.");
+    assert(merkle_branch[1] == h01, "Error in the merkle path.");
+    assert(merkle_branch[2] == h4567, "Error in the merkle path.");
+    assert(block.header.tx_hash == Block.checkMerkleBranch(h2, merkle_branch, 2), "Error in the merkle proof.");
+
+    merkle_branch = block.getMerkleBranch(4);
+    assert(merkle_branch.length == 3);
+    assert(merkle_branch[0] == h5, "Error in the merkle path.");
+    assert(merkle_branch[1] == h67, "Error in the merkle path.");
+    assert(merkle_branch[2] == h0123, "Error in the merkle path.");
+    assert(block.header.tx_hash == Block.checkMerkleBranch(h4, merkle_branch, 4), "Error in the merkle proof.");
 }
--- source/agora/test/GossipProtocol.d
+++ source/agora/test/GossipProtocol.d
@@ -32,13 +32,19 @@ unittest
     assert(network.getDiscoveredNodes().length == NodeCount);
     auto node_1 = network.apis.values[0];
 
-    KeyPair[] key_pairs = [KeyPair.random, KeyPair.random, KeyPair.random, KeyPair.random];
+    KeyPair[] key_pairs = [
+        KeyPair.random, KeyPair.random, KeyPair.random, KeyPair.random, KeyPair.random, KeyPair.random, KeyPair.random, KeyPair.random,
+        KeyPair.random, KeyPair.random, KeyPair.random, KeyPair.random, KeyPair.random, KeyPair.random, KeyPair.random, KeyPair.random,
+        KeyPair.random, KeyPair.random, KeyPair.random, KeyPair.random, KeyPair.random, KeyPair.random, KeyPair.random, KeyPair.random,
+        ];
 
-    const gen_key_pair = getGenesisKeyPair();
+    KeyPair gen_key_pair = getGenesisKeyPair();
     const gen_block = getGenesisBlock();
 
     // last transaction in the ledger
-    Hash genesis_tx_hash = hashFull(gen_block.tx);
+    Hash gen_tx_hash = hashFull(gen_block.txs[$-1]);
+    Hash last_tx_hash;
+    Transaction[] txs;
 
     // check block height
     foreach (key, ref node; network.apis)
@@ -47,106 +53,125 @@ unittest
         assert(block_height == 0, block_height.to!string);
     }
 
-    // It is validated. (the sum of `Output` == the sum of `Input`)
+    // It is validated. (the sum of `Output` < the sum of `Input`)
     // The Genesis block has 40,000,000
 
     // Creates the first transaction.
-    Transaction tx1 = Transaction(
-        [
-            Input(genesis_tx_hash, 0)
-        ],
-        [
-            Output(40_000_000, key_pairs[0].address)
-        ]
-    );
-    Hash tx1Hash = hashFull(tx1);
+    foreach (idx; 0 .. 8)
+    {
+        Transaction tx1 = Transaction(
+            [
+                Input(gen_tx_hash, 0)
+            ],
+            [
+                Output(1_000_000, key_pairs[idx].address)
+            ]
+        );
+        last_tx_hash = hashFull(tx1);
 
-    // Sign a transaction and assign it to the input
-    tx1.inputs[0].signature = gen_key_pair.secret.sign(tx1Hash[]);
-    node_1.putTransaction(tx1);
+        // Sign a transaction and assign it to the input
+        tx1.inputs[0].signature = gen_key_pair.secret.sign(last_tx_hash[]);
+        node_1.putTransaction(tx1);
+
+        txs ~= tx1;
+    }
 
     // Check hasTransactionHash
     foreach (key, ref node; network.apis)
     {
         //  every nodes received tx
-        assert(node.hasTransactionHash(tx1Hash));
+        assert(node.hasTransactionHash(last_tx_hash));
 
         auto block_height = node.getBlockHeight();
         assert(block_height == 1, block_height.to!string);
     }
 
-    // It is validated. (the sum of `Output` < the sum of `Input`)
+    // It is validated. (the sum of `Output` == the sum of `Input`)
 
     // Creates the second transaction.
-    Transaction tx2 = Transaction(
-        [
-            Input(tx1Hash, 0)
-        ],
-        [
-            Output(20_000_000, key_pairs[1].address)
-        ]
-    );
-    Hash tx2Hash = hashFull(tx2);
+    foreach (idx; 0 .. 8)
+    {
+        Transaction tx2 = Transaction(
+            [
+                Input(hashFull(txs[idx]), 0)
+            ],
+            [
+                Output(1_000_000, key_pairs[idx+8].address)
+            ]
+        );
+        last_tx_hash = hashFull(tx2);
 
-    // Sign a transaction and assign it to the input
-    tx2.inputs[0].signature = key_pairs[0].secret.sign(tx2Hash[]);
-    node_1.putTransaction(tx2);
+        // Sign a transaction and assign it to the input
+        tx2.inputs[0].signature = key_pairs[idx].secret.sign(last_tx_hash[]);
+        node_1.putTransaction(tx2);
+        txs ~= tx2;
+    }
 
     // Check hasTransactionHash
     foreach (key, ref node; network.apis)
     {
         //  every nodes received tx
-        assert(node.hasTransactionHash(tx2Hash));
+        assert(node.hasTransactionHash(last_tx_hash));
 
         auto block_height = node.getBlockHeight();
         assert(block_height == 2, block_height.to!string);
     }
 
+
     // It isn't validated. (the sum of `Output` > the sum of `Input`)
 
     // Creates the third transaction.
-    Transaction tx3 = Transaction(
-        [
-            Input(tx2Hash, 0)
-        ],
-        [
-            Output(50_000_000, key_pairs[2].address)
-        ]
-    );
-    Hash tx3Hash = hashFull(tx3);
+    foreach (idx; 0 .. 8)
+    {
+        Transaction tx3 = Transaction(
+            [
+                Input(hashFull(txs[idx+8]), 0)
+            ],
+            [
+                Output(2_000_000, key_pairs[idx].address)
+            ]
+        );
+        last_tx_hash = hashFull(tx3);
 
-    // Sign a transaction and assign it to the input
-    tx3.inputs[0].signature = key_pairs[1].secret.sign(tx3Hash[]);
-    node_1.putTransaction(tx3);
+        // Sign a transaction and assign it to the input
+        tx3.inputs[0].signature = key_pairs[idx+8].secret.sign(last_tx_hash[]);
+        node_1.putTransaction(tx3);
+        txs ~= tx3;
+    }
 
     // Check hasTransactionHash
     foreach (key, ref node; network.apis)
     {
-        assert(!node.hasTransactionHash(tx3Hash));
+        assert(!node.hasTransactionHash(last_tx_hash));
 
         auto block_height = node.getBlockHeight();
         assert(block_height == 2, block_height.to!string);
     }
 
     // Creates the fourth transaction.
-    Transaction tx4 = Transaction(
-        [
-            Input(tx2Hash, 0)
-        ],
-        [
-            Output(20_000_000, key_pairs[2].address)
-        ]
-    );
-    Hash tx4Hash = hashFull(tx4);
+    foreach (idx; 0 .. 8)
+    {
+        Transaction tx4 = Transaction(
+            [
+                Input(hashFull(txs[idx+8]), 0)
+            ],
+            [
+                Output(1_000_000, key_pairs[idx].address)
+            ]
+        );
+        last_tx_hash = hashFull(tx4);
 
-    // Sign a transaction and assign it to the input
-    tx4.inputs[0].signature = key_pairs[2].secret.sign(tx4Hash[]);
-    node_1.putTransaction(tx4);
+        // Sign a transaction and assign it to the input,  the exact `KeyPair` is `key_pairs[idx+8]
+
+        tx4.inputs[0].signature = gen_key_pair.secret.sign(last_tx_hash[]);
+        node_1.putTransaction(tx4);
+        txs ~= tx4;
+    }
 
     // Check hasTransactionHash
     foreach (key, ref node; network.apis)
     {
-        assert(!node.hasTransactionHash(tx4Hash));
+        assert(!node.hasTransactionHash(last_tx_hash));
 
         auto block_height = node.getBlockHeight();
         assert(block_height == 2, block_height.to!string);
