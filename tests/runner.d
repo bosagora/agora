7cdcfdd8a94a0129dc73027bbd3d515493fa790d
Geod24 <pro.mathias.lang@gmail.com>
2019-08-07 00:59:43 +0900

    Implement a basic integration test

---
+++ tests/runner.d
@@ -0,0 +1,83 @@
+/*******************************************************************************
+
+    Test runner for our integration test suite
+
+    Copyright:
+        Copyright (c) 2019 BOS Platform Foundation Korea
+        All rights reserved.
+
+    License:
+        MIT License. See LICENSE for details.
+
+*******************************************************************************/
+
+module runner;
+
+import std.algorithm;
+import std.array;
+import std.file;
+import std.path;
+import std.process;
+import std.stdio;
+
+/// Directory where simple tests live
+private immutable UnitPath = __FILE_FULL_PATH__.dirName.buildPath("unit");
+/// Root on which to run dub describe
+private immutable RootPath = __FILE_FULL_PATH__.dirName.dirName;
+
+private int main (string[] args)
+{
+    size_t count;
+
+    // For convenience, if no compiler is provided, uses `dmd`
+    immutable compiler = args.length >= 2 ? args[1] : "dmd";
+
+    const importPaths = getImportPaths();
+    const lflags = getLflags();
+    if (!importPaths.length || !lflags.length)
+        return 1;
+    const Args = ["rdmd", "--compiler=" ~ compiler, "-cov", ] ~
+        importPaths.map!(v => "-I" ~ v).array ~
+        lflags.map!(v => "-L" ~ v).array;
+
+    foreach (test; dirEntries(UnitPath, SpanMode.shallow))
+    {
+        writeln("Running test on ", test);
+        auto pp = pipeProcess(Args ~ test);
+        if (pp.pid.wait() != 0)
+        {
+            pp.stdout.byLine.each!(a => writeln("[stdout]\t", a));
+            pp.stderr.byLine.each!(a => writeln("[stderr]\t", a));
+            writeln("Test ", test, " failed!");
+            return 1;
+        }
+
+        count++;
+    }
+    writeln("Ran ", count, " tests");
+    return count != 0 ? 0 : 1;
+}
+
+/// Get the import path from dub
+private string[] getImportPaths ()
+{
+    auto pp = pipeProcess(["dub", "--root=" ~ RootPath, "describe", "--import-paths"]);
+    if (pp.pid.wait() != 0)
+    {
+        pp.stderr.byLine.each!(a => writeln("[fatal]\t", a));
+        return null;
+    }
+    return pp.stdout.byLineCopy.array;
+}
+
+/// Get the linker flags from dub
+private string[] getLflags ()
+{
+    auto pp = pipeProcess(["dub", "--root=" ~ RootPath, "describe", "--data=lflags", "--data-list"]);
+    if (pp.pid.wait() != 0)
+    {
+        pp.stderr.byLine.each!(a => writeln("[fatal]\t", a));
+        return null;
+    }
+    return pp.stdout.byLineCopy.array;
+}
