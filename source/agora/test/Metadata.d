7bb0c41c5a0d0855e38353893e9e0c58419a7030
Andrej Mitrovic <andrej.mitrovich@gmail.com>
2019-08-13 17:02:04 +0900

    Get rid of the MetaTestNetwork
    It was only overriding the start() method
    in order to inject peer metadata.
    
    This is now a separate method inside of
    NetworkManager which should be called before
    the start() method.

--- source/agora/test/Metadata.d
+++ source/agora/test/Metadata.d
@@ -27,35 +27,11 @@ import std.array;
 import std.algorithm;
 
 ///
-class MetaAPIManager : TestAPIManager
-{
-    static import std.concurrency;
-    import geod24.LocalRest;
-
-    /// before we start the nodes, we want to simulate metadata configuration
-    public override void start ()
-    {
-        auto keys = this.apis.keys.array;
-
-        foreach (key_x; keys)
-        foreach (key_y; keys)
-        {
-            if (key_x == key_y)
-                continue;
-
-            this.apis[key_x].metaAddPeer(key_y.toString());
-            this.apis[key_y].metaAddPeer(key_x.toString());
-        }
-
-        super.start();
-    }
-}
-
-///
 unittest
 {
     const NodeCount = 4;
-    auto network = makeTestNetwork!MetaAPIManager(NetworkTopology.Simple, NodeCount, false);
+    auto network = makeTestNetwork(NetworkTopology.Simple, NodeCount, false);
+    network.addMetadata();
     network.start();
     scope(exit) network.shutdown();
     assert(network.getDiscoveredNodes().length == NodeCount);
