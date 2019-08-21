23fd2e1a052161be5a311661b29438fe3135d8e1
Geod24 <pro.mathias.lang@gmail.com>
2019-08-21 17:00:35 +0900

    Add a system integration test based on docker-compose
    The system integration test spawns a network of 3 nodes
    using `docker-compose`.
    Eight transactions are sent, and we verify a block
    has been created as a result.

---
+++ tests/system/source/main.d
@@ -0,0 +1,91 @@
+/*******************************************************************************
+
+    Stand alone client to test basic functionalities of the node
+
+    Copyright:
+        Copyright (c) 2019 BOS Platform Foundation Korea
+        All rights reserved.
+
+    License:
+        MIT License. See LICENSE for details.
+
+*******************************************************************************/
+
+module main;
+
+import agora.consensus.data.Transaction;
+import agora.consensus.Genesis;
+import agora.common.crypto.Key;
+import agora.common.Data;
+import agora.common.Hash;
+import agora.node.API;
+
+import vibe.web.rest;
+import std.stdio;
+import core.thread;
+import core.time;
+
+private immutable Addrs = [
+    "http://127.0.0.1:4000",
+    "http://127.0.0.1:4001",
+    "http://127.0.0.1:4002"
+];
+
+void main ()
+{
+    API[3] clients;
+    foreach (idx, addr; Addrs)
+        clients[idx] = new RestInterfaceClient!API(addr);
+
+    foreach (idx, ref client; clients)
+    {
+        writefln("[%s] getPublicKey: %s", idx, client.getPublicKey());
+        writefln("[%s] getNetworkInfo: %s", idx, client.getNetworkInfo());
+        const height = client.getBlockHeight();
+        writefln("[%s] getBlockHeight: %s", idx, height);
+        writeln("----------------------------------------");
+        assert(height == 0);
+    }
+
+    auto kp = getGenesisKeyPair();
+    const gTx = getGenesisTx();
+    const gTxH = hashFull(gTx);
+
+    foreach (idx; 0 .. 8)
+    {
+        auto tx = Transaction(
+            [Input(gTxH, idx)],
+            [Output(gTx.outputs[idx].value, kp.address)]
+        );
+
+        auto signature = kp.secret.sign(hashFull(tx)[]);
+        tx.inputs[0].signature = signature;
+        clients[0].putTransaction(tx);
+    }
+
+    Thread.sleep(1.seconds);
+
+    Hash blockHash;
+    foreach (idx, ref client; clients)
+    {
+        const height = client.getBlockHeight();
+        const blocks = client.getBlocksFrom(0, 42);
+        writefln("[%s] getBlockHeight: %s", idx, height);
+        writefln("[%s] getBlocksFrom: %s", idx, blocks);
+        writeln("----------------------------------------");
+        assert(height == 1);
+        assert(blocks.length == 2);
+        if (idx != 0)
+            assert(blockHash == hashFull(blocks[1].header));
+        else
+            blockHash = hashFull(blocks[1].header);
+    }
+}
+
+/// Copied from Agora
+public KeyPair getGenesisKeyPair ()
+{
+    return KeyPair.fromSeed(
+        Seed.fromString(
+            "SCT4KKJNYLTQO4TVDPVJQZEONTVVW66YLRWAINWI3FZDY7U4JS4JJEI4"));
+}
