28d5fecdeff24351044ec5eb4f77c33a65c44d73
Andrej Mitrovic <andrej.mitrovich@gmail.com>
2020-01-15 10:45:11 +0900

    Test out some ulimit settings.

--- ci/run.sh
+++ ci/run.sh
@@ -3,6 +3,10 @@
 set -xeu
 set -o pipefail
 
+ulimit -Ss 16384
+ulimit -Hs
+ulimit -Ss
+
 dub test -b unittest-cov --skip-registry=all --compiler=${DC}
 rdmd --compiler=${DC} ./tests/runner.d --compiler=${DC} -cov
 dub build --skip-registry=all --compiler=${DC}
