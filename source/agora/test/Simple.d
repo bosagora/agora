96efa4b08413d89a102fd907e2ce06b06428c7ab
Andrej Mitrovic <andrej.mitrovich@gmail.com>
2020-02-06 13:34:59 +0900

    Add a simple test to the test-suite
    It's useful to be able to test functionality of new
    features without running the entire test-suite.
    
    And it serves as a good template test-case,
    to build more elaborate test-cases from.
    
    Running the simple test is easy:
    
    $ dtest=agora.test.Simple dub test

---
+++ source/agora/test/Simple.d
@@ -0,0 +1,50 @@
+/*******************************************************************************
+
+    Contains the simplest possible block creating test. Useful as a starting
+    point for creating more complext test-cases, or just to test new behavior
+    with the simplest creation of blocks.
+
+    Run via:
+    $ dtest=agora.test.Simple dub test
+
+    Copyright:
+        Copyright (c) 2020 BOS Platform Foundation Korea
+        All rights reserved.
+
+    License:
+        MIT License. See LICENSE for details.
+
+*******************************************************************************/
+
+module agora.test.Simple;
+
+version (unittest):
+
+import agora.api.Validator;
+import agora.consensus.data.Transaction;
+import agora.consensus.Genesis;
+import agora.test.Base;
+
+/// Simple test
+unittest
+{
+    import std.algorithm;
+    import std.range;
+    import core.thread;
+
+    const NodeCount = 4;
+    auto network = makeTestNetwork(NetworkTopology.Simple, NodeCount);
+    network.start();
+    scope(exit) network.shutdown();
+    scope(failure) network.printLogs();
+    assert(network.getDiscoveredNodes().length == NodeCount);
+
+    auto nodes = network.apis.values;
+    auto node_1 = nodes[0];
+
+    auto txes = makeChainedTransactions(getGenesisKeyPair(), null, 1);
+    txes.each!(tx => node_1.putTransaction(tx));
+
+    nodes.all!(node => node.getBlockHeight() == 1)
+        .retryFor(2.seconds);
+}
