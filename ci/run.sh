d4b9f95027e0398728f34cc450b893b987742ff1
Hyeonyeob Kim <hyeonyeob.kim@bpfkorea.org>
2020-02-18 15:31:49 +0900

    Adds CI to check the Agora-Cli compilation.

--- ci/run.sh
+++ ci/run.sh
@@ -6,3 +6,4 @@ set -o pipefail
 dub test -b unittest-cov --skip-registry=all --compiler=${DC}
 rdmd --compiler=${DC} ./tests/runner.d --compiler=${DC} -cov
 dub build --skip-registry=all --compiler=${DC}
+dub build -c cli --skip-registry=all --compiler=${DC}
