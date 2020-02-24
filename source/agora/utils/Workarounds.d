d6038dc1295ca1003d87a0c762aa064c35d5ae50
Geod24 <pro.mathias.lang@gmail.com>
2020-02-24 15:39:02 +0900

    Move compiler workaround to its own module
    This will allow code in tests/unit/ to import it,
    if they themselves import modules in Agora that require it.

---
+++ source/agora/utils/Workarounds.d
@@ -0,0 +1,24 @@
+/*******************************************************************************
+
+    Workarounds for compiler / runtime / upstream issues
+
+    Live in this module so they can be imported by code that imports other
+    module in Agora, such as the system integration test.
+
+    Copyright:
+        Copyright (c) 2019 BOS Platform Foundation Korea
+        All rights reserved.
+
+    License:
+        MIT License. See LICENSE for details.
+
+*******************************************************************************/
+
+module agora.utils.Workarounds;
+
+/**
+ * Workaround for segfault similar (or identical) to https://github.com/dlang/dub/issues/1812
+ * https://dlang.org/changelog/2.087.0.html#gc_parallel
+ */
+static if (__VERSION__ >= 2087)
+    extern(C) __gshared string[] rt_options = [ "gcopt=parallel:0" ];
