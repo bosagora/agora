3a605c0bfe2932477b7db4b5bcfb61d5f6c1759e
Andrej Mitrovic <andrej.mitrovich@gmail.com>
2020-03-16 17:41:56 +0900

    Add rudimentary bindings to std::unordered_map
    It's used heavily in the quorum intersection
    checker code which will be binded to soon.

--- source/scpp/extra/DUtils.h
+++ source/scpp/extra/DUtils.h
@@ -4,7 +4,10 @@
 // Not originally part of SCP but required for the D side to work
 
 #include <set>
+#include <unordered_map>
 #include <vector>
+
+#include "crypto/SecretKey.h"  // for operator() (hashing support)
 
 // rudimentary support for walking through an std::set
 // note: can't use proper callback type due to
@@ -29,6 +32,34 @@ bool cpp_set_empty(const void* setptr)
 {
     return ((const std::set<T>*)setptr)->empty();
 }
+
+template<typename K, typename V>
+void cpp_unordered_map_assign (void* map, const K& key, const V& value)
+{
+    auto m = (std::unordered_map<K, V>*)map;
+    (*m)[key] = value;
+}
+
+template<typename K, typename V>
+std::size_t cpp_unordered_map_length (const void* map)
+{
+    auto m = (const std::unordered_map<K, V>*)map;
+    return m->size();
+}
+
+template<typename K, typename V>
+void* cpp_unordered_map_create ()
+{
+    return new std::unordered_map<K, V>();
+}
+
+// @bug with substitution
+// https://issues.dlang.org/show_bug.cgi?id=20679
+// template<typename K, typename V>
+// std::unordered_map<K, V> * cpp_unordered_map_create ()
+// {
+//     return new std::unordered_map<K, V>();
+// }
 
 template<typename T, typename VectorT>
 void push_back(VectorT& this_, T& value)
