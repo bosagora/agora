ac2714a28ab3e85209860f8f75916ccc7f0a6aa7
Hyeonyeob Kim <hyeonyeob.kim@bpfkorea.org>
2020-04-06 18:04:38 +0900

    Correct the Github actions CI Test to return the error level correctly
    A build test error occurred in the window cmd environment, but the exit code is not returned.
    Returns only the last run result.

--- ci/run.bat
+++ ci/run.bat
@@ -1,4 +1,8 @@
 call dub test --skip-registry=all --compiler=%DC%
+::if %errorlevel% neq 0 exit /b %errorlevel%
 call rdmd --compiler=%DC% ./tests/runner.d --compiler=%DC% -cov
+::if %errorlevel% neq 0 exit /b %errorlevel%
 call dub build --skip-registry=all --compiler=%DC%
+if %errorlevel% neq 0 exit /b %errorlevel%
 call dub build -c client --skip-registry=all --compiler=%DC%
+if %errorlevel% neq 0 exit /b %errorlevel%
