085ec53a9695b2317577c8b8dc26c1eee265dedd
Hyunjae Lee <linked0@gmail.com>
2020-06-04 16:11:11 +0900

    Add `ConsensusParams` class for consensus-critical constants
    For unit-tests, it's not efficient to test with the constants for consensus
    like the `ValidatorCycle` because the original value make the tests slower and
    very time-consuming. The goal of unit tests is checking the operations run
    properly in test environment.
    
    In this commit, we provide the definition for configurable consensus-critical
    constants, or the `ConsensusParams`.

---
+++ source/agora/consensus/data/ConsensusParams.d
@@ -0,0 +1,50 @@
+/*******************************************************************************
+
+    The set for consensus-critical constants
+
+    This defines the class for the consensus-critical constants. Only one
+    object should exist for a single node. The `class` is `immutable`, hence
+    the constants need to be set at the start of the process. The
+    consensus-critical constants are the protocol-level constants, so they
+    shouldn't be modified outside of test environments.
+
+    Copyright:
+        Copyright (c) 2019-2020 BOS Platform Foundation Korea
+        All rights reserved.
+
+    License:
+        MIT License. See LICENSE for details.
+
+*******************************************************************************/
+
+module agora.consensus.data.ConsensusParams;
+
+/// Ditto
+public immutable class ConsensusParams
+{
+    /// The cycle length for a validator
+    public uint ValidatorCycle;
+
+    /***************************************************************************
+
+        Constructor
+
+        Params:
+            validator_cycle = cycle length for a validator
+
+    ***************************************************************************/
+
+    public this (uint validator_cycle = 1008)
+    {
+        this.ValidatorCycle = validator_cycle;
+    }
+}
+
+unittest
+{
+    auto params = new immutable(ConsensusParams)(100);
+    assert(params.ValidatorCycle == 100);
+
+    auto params2 = new immutable(ConsensusParams)();
+    assert(params2.ValidatorCycle == 1008);
+}
