dd84a839910444b22719a5ef5d3fd905a14ff871
Andrej Mitrovic <andrej.mitrovich@gmail.com>
2020-07-06 15:27:38 +0900

    Emit unittested modules in the CI
    Using a 'dchatty' environment variable.

--- ci/run.sh
+++ ci/run.sh
@@ -3,7 +3,7 @@
 set -xeu
 set -o pipefail
 
-dub test -b unittest-cov --skip-registry=all --compiler=${DC}
+dchatty=1 dub test -b unittest-cov --skip-registry=all --compiler=${DC}
 rdmd --compiler=${DC} ./tests/runner.d --compiler=${DC} -cov
 dub build --skip-registry=all --compiler=${DC}
 dub build -c client --skip-registry=all --compiler=${DC}
