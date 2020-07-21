ecf0ebfd2a85604316fcf4123982a644ec68888c
Geod24 <pro.mathias.lang@gmail.com>
2020-07-21 17:04:56 +0900

    CI: Implement a script-side timeout and retry mechanism
    Currently the CI is extremely flakey and that affects development speed.
    This is a band-aid in an attempt to reduce the failures.

--- ci/run.sh
+++ ci/run.sh
@@ -3,7 +3,15 @@
 set -xeu
 set -o pipefail
 
-dchatty=1 dub test -b unittest-cov --skip-registry=all --compiler=${DC}
+# Only build the unittest binary, but don't run it
+# We want to run it ourselves to catch any bug / set timeout, etc...
+dub build -b unittest-cov -c unittest --skip-registry=all --compiler=${DC}
+
+dchatty=1
+# A run currently (2020-07-21) takes < 6 minutes on Linux
+# Try a total of three times
+timeout -s SEGV 8m ./build/agora-unittests || timeout -s SEGV 8m ./build/agora-unittests || timeout -s SEGV 8m ./build/agora-unittests
+
 rdmd --compiler=${DC} ./tests/runner.d --compiler=${DC} -cov
 dub build --skip-registry=all --compiler=${DC}
 dub build -c client --skip-registry=all --compiler=${DC}
