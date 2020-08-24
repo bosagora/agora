b41caebc7f2bcf9d6eb218ffc52fc4b2055ba89c
Geod24 <pro.mathias.lang@gmail.com>
2020-08-24 09:22:48 +0900

    Remove retry timeout
    This was put in place to deal with the frequent infinite loop we were seeing.
    Remove it now that those have been fixed so they don't creep back.

--- ci/run.sh
+++ ci/run.sh
@@ -8,9 +8,7 @@ set -o pipefail
 dub build -b unittest-cov -c unittest --skip-registry=all --compiler=${DC}
 
 export dchatty=1
-# A run currently (2020-07-21) takes < 6 minutes on Linux
-# Try a total of three times
-timeout -s SEGV 8m ./build/agora-unittests || timeout -s SEGV 8m ./build/agora-unittests || timeout -s SEGV 8m ./build/agora-unittests
+./build/agora-unittests
 
 rdmd --compiler=${DC} ./tests/runner.d --compiler=${DC} -cov
 dub build --skip-registry=all --compiler=${DC}
