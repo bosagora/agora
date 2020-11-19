92179ed040a77d41e52f67f968fc096fbfc0e0d1
Andrej Mitrovic <andrej.mitrovich@gmail.com>
2020-11-19 19:28:08 +0900

    Replace stack.count() with stack.empty()

--- source/agora/script/Engine.d
+++ source/agora/script/Engine.d
@@ -495,7 +495,7 @@ public class Engine
                 break;
 
             case OP.VERIFY_LOCK_HEIGHT:
-                if (stack.count() < 1)
+                if (stack.empty())
                     return "VERIFY_LOCK_HEIGHT opcode requires a lock height on the stack";
 
                 const height_bytes = stack.pop();
@@ -510,7 +510,7 @@ public class Engine
                 break;
 
             case OP.VERIFY_UNLOCK_AGE:
-                if (stack.count() < 1)
+                if (stack.empty())
                     return "VERIFY_UNLOCK_AGE opcode requires an unlock age on the stack";
 
                 const age_bytes = stack.pop();
@@ -580,7 +580,7 @@ public class Engine
                 break;
             }
 
-            if (stack.count() < 1)
+            if (stack.empty())
                 return "IF/NOT_IF opcode requires an item on the stack";
 
             const top = stack.pop();
