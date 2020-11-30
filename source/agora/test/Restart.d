319741e5debc5842c1172d07e5f2c1c4e649814b
Geod24 <pro.mathias.lang@gmail.com>
2020-11-30 10:54:08 +0100

    Add a restart test that use persistence
    Make sure that when we restart and there is data on disk,
    we don't outright crash, as that's not the expected behavior.

--- source/agora/test/Restart.d
+++ source/agora/test/Restart.d
@@ -16,8 +16,14 @@ module agora.test.Restart;
 version (unittest):
 
 import agora.api.Validator;
-import agora.consensus.data.Transaction;
+import agora.common.Config;
+import agora.consensus.data.Block;
+import agora.consensus.data.Params;
 import agora.test.Base;
+
+import geod24.Registry;
+
+import core.stdc.time;
 
 /// A test that stops and restarts a node
 unittest
@@ -38,6 +44,91 @@ unittest
 
     // Now shut down & restart one node
     auto restartMe = nodes[$-1];
+    network.restart(restartMe);
+    network.waitForDiscovery();
+    network.expectBlock(Height(1));
+}
+
+/// Node which has a persistent Ledger (restart always clear the local state)
+private class PersistentNode : TestValidatorNode
+{
+    import agora.consensus.EnrollmentManager;
+    import agora.consensus.state.UTXODB;
+    import agora.node.BlockStorage;
+
+    mixin ForwardCtor!();
+
+    ///
+    private static UTXOSet utxo_set_saved;
+    ///
+    private static EnrollmentManager em_saved;
+    ///
+    private static IBlockStorage blockstorage_saved;
+
+    ///
+    protected override IBlockStorage getBlockStorage (string data_dir) @system
+    {
+        if (blockstorage_saved is null)
+            blockstorage_saved = super.getBlockStorage(data_dir);
+        return blockstorage_saved;
+    }
+
+    ///
+    protected override UTXOSet getUtxoSet (string data_dir)
+    {
+        if (utxo_set_saved is null)
+            utxo_set_saved = super.getUtxoSet(data_dir);
+        return utxo_set_saved;
+    }
+
+    ///
+    protected override EnrollmentManager getEnrollmentManager (
+        string data_dir, in ValidatorConfig validator_config,
+        immutable(ConsensusParams) params)
+    {
+        if (em_saved is null)
+            em_saved = super.getEnrollmentManager(data_dir, validator_config, params);
+        return em_saved;
+    }
+}
+
+private class WithPersistentNodeAPIManager : TestAPIManager
+{
+    ///
+    public this (immutable(Block)[] blocks, TestConf test_conf, time_t genesis_start_time)
+    {
+        super(blocks, test_conf, genesis_start_time);
+    }
+
+    public override void createNewNode (Config conf,
+        string file = __FILE__, int line = __LINE__)
+    {
+        if (conf.validator.enabled)
+            this.addNewNode!PersistentNode(conf, file, line);
+        else
+            super.createNewNode(conf, file, line);
+    }
+}
+
+/// Stops and restarts a node with a pre-existing state
+unittest
+{
+    TestConf conf = TestConf.init;
+    auto network = makeTestNetwork!WithPersistentNodeAPIManager(conf);
+    network.start();
+    scope(exit) network.shutdown();
+    network.waitForDiscovery();
+
+    auto nodes = network.clients;
+    auto node_1 = nodes[0];
+
+    auto txes = genesisSpendable().map!(txb => txb.sign()).array();
+    txes.each!(tx => node_1.putTransaction(tx));
+    network.expectBlock(Height(1));
+
+    // Now shut down & restart one node
+    auto restartMe = nodes[$-1];
+    scope(failure) restartMe.printLog();
     network.restart(restartMe);
     network.waitForDiscovery();
     network.expectBlock(Height(1));
