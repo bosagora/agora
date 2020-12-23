2409b788471361d813a0f3113bc2aaed94907d95
Chris Hewison <hewisonchris@gmail.com>
2020-12-23 10:09:12 +0900

    ci run update. singlethreaded test run

--- ci/run.sh
+++ ci/run.sh
@@ -3,15 +3,18 @@
 set -xeu
 set -o pipefail
 
+dub build --skip-registry=all --compiler=${DC}
+dub build -c client --skip-registry=all --compiler=${DC}
+
 # Only build the unittest binary, but don't run it
 # We want to run it ourselves to catch any bug / set timeout, etc...
 dub build -b unittest-cov -c unittest --skip-registry=all --compiler=${DC}
 
-export dchatty=1
-# A run currently (2020-07-21) takes < 6 minutes on Linux
-# Try a total of three times
-timeout -s SEGV 8m ./build/agora-unittests || timeout -s SEGV 8m ./build/agora-unittests || timeout -s SEGV 8m ./build/agora-unittests
-
+# Run this after unit tests have proven to compile ok
 rdmd --compiler=${DC} ./tests/runner.d --compiler=${DC} -cov
-dub build --skip-registry=all --compiler=${DC}
-dub build -c client --skip-registry=all --compiler=${DC}
+
+export dchatty=1
+export dsinglethreaded=1
+# A run currently (2020-07-21) takes < 6 minutes on Linux
+# Run a single test at a time to prevent resource issues and also see which test failed
+timeout -s SEGV 15m ./build/agora-unittests
