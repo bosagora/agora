558dbd503da5e8bfe83bd941cafc56d76c2aa1b6
Geod24 <pro.mathias.lang@gmail.com>
2021-01-22 18:03:44 +0900

    Re-enable parallel GC
    It should work, as the oldest supported version is LDC v1.20, which has the fix.

--- source/agora/utils/Workarounds.d
+++ source/agora/utils/Workarounds.d
@@ -25,10 +25,3 @@ version (CRuntime_Musl)
         Runtime.traceHandler = &libunwindDefaultTraceHandler;
     }
 }
-
-/**
- * Workaround for segfault similar (or identical) to https://github.com/dlang/dub/issues/1812
- * https://dlang.org/changelog/2.087.0.html#gc_parallel
- */
-static if (__VERSION__ >= 2087)
-    extern(C) __gshared string[] rt_options = [ "gcopt=parallel:0" ];
