e5d1d7194f9b4f1b91ecd588262bf1290a4ee6f3
Geod24 <pro.mathias.lang@gmail.com>
2021-01-22 14:52:12 +0900

    Trivial: Remove unused import in Validator

--- source/agora/node/Validator.d
+++ source/agora/node/Validator.d
@@ -20,7 +20,6 @@ import agora.common.Hash;
 import agora.common.crypto.Key;
 import agora.common.Set;
 import agora.common.Task;
-import agora.node.TransactionPool;
 import agora.common.Types;
 import agora.consensus.data.Block;
 import agora.consensus.data.Params;
