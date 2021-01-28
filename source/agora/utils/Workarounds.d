4921210975f8a98d34b8eb6f7ac088c6a9211862
Geod24 <pro.mathias.lang@gmail.com>
2021-01-28 18:42:55 +0900

    Revert "Re-enable parallel GC"
    This reverts commit 87398d03d4893b376b108a3eae11d3e5f1043d96.

--- source/agora/utils/Workarounds.d
+++ source/agora/utils/Workarounds.d
@@ -25,3 +25,10 @@ version (CRuntime_Musl)
         Runtime.traceHandler = &libunwindDefaultTraceHandler;
     }
 }
+
+/**
+ * Workaround for segfault similar (or identical) to https://github.com/dlang/dub/issues/1812
+ * https://dlang.org/changelog/2.087.0.html#gc_parallel
+ */
+static if (__VERSION__ >= 2087)
+    extern(C) __gshared string[] rt_options = [ "gcopt=parallel:0" ];
