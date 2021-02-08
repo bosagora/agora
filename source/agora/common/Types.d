22adba052439fcc94553bb29c26a24f7fe2f9b62
Geod24 <pro.mathias.lang@gmail.com>
2021-02-08 09:32:55 +0900

    Fix #1221 - Split crypto code into its own module

--- source/agora/common/Types.d
+++ source/agora/common/Types.d
@@ -14,8 +14,7 @@
 module agora.common.Types;
 
 import agora.common.crypto.Key;
-
-import geod24.bitblob;
+public import agora.crypto.Types;
 
 /// Represents a specific point in time, it should be changed to time_t
 /// after time_t became platform independent
@@ -24,38 +23,14 @@ public alias TimePoint = ulong;
 /// An array of const characters
 public alias cstring = const(char)[];
 
-/// 512 bits hash type computed via `BLAKE2b`
-public alias Hash = BitBlob!512;
-
 /// A network address
 public alias Address = string;
-
-/// The type of a signature
-public alias Signature = BitBlob!512;
 
 /// Whether integers are serialized in variable-length form
 public enum CompactMode : bool
 {
     No,
     Yes
-}
-
-/// Describe print modes for `toString`
-public enum PrintMode
-{
-    /// Print hidden version
-    Obfuscated,
-    /// Print original value
-    Clear,
-}
-
-unittest
-{
-    // Check that our type match libsodium's definition
-    import libsodium;
-
-    static assert(Signature.sizeof == crypto_sign_ed25519_BYTES);
-    static assert(Hash.sizeof == crypto_generichash_BYTES_MAX);
 }
 
 /// The definition of a Quorum
---
+++ source/agora/test/Crypto.d
@@ -0,0 +1,57 @@
+/*******************************************************************************
+
+    Contains extra tests for the `crypto` library
+
+    Those tests do not belong to the crypto library, but ensures that it
+    integrates correctly with utilities we use.
+
+    Copyright:
+        Copyright (c) 2019 BOS Platform Foundation Korea
+        All rights reserved.
+
+    License:
+        MIT License. See LICENSE for details.
+
+*******************************************************************************/
+
+module agora.test.Crypto;
+
+import agora.common.Serializer;
+import agora.crypto.Types;
+import agora.crypto.ECC;
+import agora.crypto.Schnorr;
+
+unittest
+{
+    auto s = Scalar("0x0e00a8df701806cb4deac9bb09cc85b097ee713e055b9d2bf1daf668b3f63778");
+
+    // Test default formatting behavior with Ocean's sformat/log
+    // The library test integration with Phobos
+    import ocean.text.convert.Formatter : format;
+    assert(format("{}", s) == "**SCALAR**");
+
+    import vibe.data.json;
+    assert(s.serializeToJsonString() == "\"**SCALAR**\"",
+           s.serializeToJsonString());
+}
+
+// Test serialization for types in `agora.crypto.ECC`
+unittest
+{
+    import agora.common.Serializer;
+    testSymmetry!Scalar();
+    testSymmetry(Scalar.random());
+    testSymmetry!Point();
+    testSymmetry(Scalar.random().toPoint());
+    // Make sure it's serialized as a value type (without length)
+    assert(Scalar.random().toPoint().serializeFull().length == Point.sizeof);
+}
+
+// Test serialization for types in `agora.crypto.Schnorr`
+unittest
+{
+    const KP = Pair.random();
+    auto signature = Sig(KP.V, KP.v).toBlob();
+    auto bytes = signature.serializeFull();
+    assert(bytes.deserializeFull!Signature == signature);
+}
