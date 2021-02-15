a6183738fceb8f360294936b350653d0c51e3b4e
Geod24 <pro.mathias.lang@gmail.com>
2021-02-15 13:24:35 +0900

    PeriodicCatchup: Clean up test dependencies

--- source/agora/test/PeriodicCatchup.d
+++ source/agora/test/PeriodicCatchup.d
@@ -17,43 +17,17 @@ module agora.test.PeriodicCatchup;
 
 version (unittest):
 
-import agora.api.Validator;
 import agora.common.Config;
-import agora.common.Task;
 import agora.common.Types;
-import agora.common.crypto.Key;
-import agora.consensus.data.Block;
-import agora.consensus.EnrollmentManager;
-import agora.consensus.data.Params;
-import agora.consensus.data.Transaction;
-import agora.consensus.data.ValidatorBlockSig;
-import agora.consensus.protocol.Data;
-import agora.consensus.protocol.Nominator;
-import agora.network.Clock;
-import agora.network.NetworkClient;
-import agora.network.NetworkManager;
-import agora.node.Ledger;
 import agora.test.Base;
-import agora.utils.SCPPrettyPrinter;
 import agora.utils.Log;
 import agora.utils.PrettyPrinter;
 
-
 import scpd.types.Stellar_SCP;
-import scpd.types.Stellar_types : NodeID;
 
-import geod24.Registry;
-
-import std.algorithm;
 import std.exception;
-import std.format;
-import std.range;
-import std.stdio;
-
-import core.exception;
 import core.stdc.inttypes;
 import core.thread;
-import core.atomic;
 
 mixin AddLogger!();
 
@@ -95,10 +69,7 @@ private class TestNode () : TestValidatorNode
 private class NodeManager (): TestAPIManager
 {
     ///
-    public this (immutable(Block)[] blocks, TestConf test_conf, TimePoint genesis_start_time)
-    {
-        super(blocks, test_conf, genesis_start_time);
-    }
+    mixin ForwardCtor!();
 
     public override void createNewNode (Config conf,
         string file = __FILE__, int line = __LINE__)
