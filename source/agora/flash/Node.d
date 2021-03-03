db75de44a6962c8d8e80933d408c7e79ece075df
Andrej Mitrovic <andrej.mitrovich@gmail.com>
2021-03-03 12:29:05 +0900

    Fix shadowing variable

--- source/agora/flash/Node.d
+++ source/agora/flash/Node.d
@@ -476,8 +476,8 @@ public abstract class FlashNode : FlashAPI
         }
         else if (error)
         {
-            foreach (id, channel; this.channels)
-                channel.learnSecrets([], [payment_hash], this.last_block_height);
+            foreach (id, chan; this.channels)
+                chan.learnSecrets([], [payment_hash], this.last_block_height);
             this.reportPaymentError(chan_id, OnionError(Hash.init,
                 payment_hash, chan_id, error));
         }
