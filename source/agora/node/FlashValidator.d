7e757878d87193f32fe3ee7d23d7e23bf12c9f5c
Andrej Mitrovic <andrej.mitrovich@gmail.com>
2021-03-10 15:56:53 +0900

    Flash: Reduce code duplication with mixin
    Introduce 'FlashNodeCommon' to avoid duplicating
    functionality between FlashFullNode / FlashValidator.
    
    Using mixin in place of inheritance as multiple inheritance
    is not supported by D.

--- source/agora/node/FlashValidator.d
+++ source/agora/node/FlashValidator.d
@@ -44,6 +44,7 @@ import agora.network.Clock;
 import agora.network.Manager;
 import agora.node.admin.AdminInterface;
 import agora.node.BlockStorage;
+import agora.node.FlashFullNode;
 import agora.node.FullNode;
 import agora.node.Ledger;
 import agora.node.Validator;
@@ -53,6 +54,9 @@ import agora.utils.PrettyPrinter;
 import agora.utils.Test;
 
 import scpd.types.Stellar_SCP;
+
+import vibe.data.json;
+import vibe.web.rest;
 
 import std.algorithm : each, map;
 import std.exception;
@@ -89,28 +93,6 @@ public class FlashValidator : Validator, FlashValidatorAPI
             this.config.validator.key_pair.secret.toPoint);
         this.flash = new AgoraFlashNode(kp, hashFull(this.params.Genesis),
             this.engine, this.taskman, this, &this.getFlashClient);
-    }
-
-    private ExtendedFlashAPI getFlashClient (in Point peer_pk, Duration timeout)
-    {
-        // todo: need to retry later
-        // todo: need a key => IP mapping (maybe through the NameRegistryAPI?)
-        auto pk = PublicKey(peer_pk[]);
-        log.info("getFlashClient searching peer: {}", pk);
-        auto ip = this.network.getAddress(pk);
-        enforce(ip !is null, "Could not find mapping of key => IP");
-
-        import vibe.http.client;
-
-        import vibe.web.rest;
-        auto settings = new RestInterfaceSettings;
-        // todo: this is obviously wrong, need proper connection handling later
-        settings.baseURL = URL(ip);
-        settings.httpClientSettings = new HTTPClientSettings;
-        settings.httpClientSettings.connectTimeout = timeout;
-        settings.httpClientSettings.readTimeout = timeout;
-
-        return new RestInterfaceClient!ExtendedFlashAPI(settings);
     }
 
     public override void start ()
@@ -324,117 +306,5 @@ public class FlashValidator : Validator, FlashValidatorAPI
         // then use the extended flash API to send the invoice
     }
 
-    /***************************************************************************
-
-        Called when a block was externalized.
-
-        Calls pushBlock(), but additionally the Validator overrides this
-        and implements quorum shuffling.
-
-        Params:
-            block = the new block
-            validators_changed = whether the validator set has changed
-
-    ***************************************************************************/
-
-    protected override void onAcceptedBlock (in Block block,
-        bool validators_changed) @safe
-    {
-        super.onAcceptedBlock(block, validators_changed);
-        this.flash.onExternalizedBlock(block);
-    }
-
-
-    ///
-    public override Result!PublicNonce openChannel (
-        /* in */ ChannelConfig chan_conf,
-        /* in */ PublicNonce peer_nonce) @trusted
-    {
-        return this.flash.openChannel(chan_conf, peer_nonce);
-    }
-
-    ///
-    public override Result!Point closeChannel (/* in */ Hash chan_id,
-        /* in */ uint seq_id, /* in */ Point peer_nonce, /* in */ Amount fee)
-        @trusted
-    {
-        return this.flash.closeChannel(chan_id, seq_id, peer_nonce, fee);
-    }
-
-    ///
-    public override void gossipChannelsOpen (ChannelConfig[] chan_configs)
-        @trusted
-    {
-        this.flash.gossipChannelsOpen(chan_configs);
-    }
-
-    ///
-    public override void gossipChannelUpdates (ChannelUpdate[] chan_updates)
-        @trusted
-    {
-        this.flash.gossipChannelUpdates(chan_updates);
-    }
-
-    ///
-    public override Result!ChannelState getChannelState (/* in */ Hash chan_id)
-        @trusted
-    {
-        return this.flash.getChannelState(chan_id);
-    }
-
-    ///
-    public override Result!PublicNonce proposePayment (/* in */ Hash chan_id,
-        /* in */ uint seq_id, /* in */ Hash payment_hash,
-        /* in */ Amount amount, /* in */ Height lock_height,
-        /* in */ OnionPacket packet, /* in */ PublicNonce peer_nonce,
-        /* in */ Height height) @trusted
-    {
-        return this.flash.proposePayment(chan_id, seq_id, payment_hash,
-            amount, lock_height, packet, peer_nonce, height);
-    }
-
-    ///
-    public override Result!PublicNonce proposeUpdate (/* in */ Hash chan_id,
-        /* in */ uint seq_id, /* in */ Hash[] secrets,
-        /* in */ Hash[] rev_htlcs, /* in */ PublicNonce peer_nonce,
-        /* in */ Height block_height) @trusted
-    {
-        return this.flash.proposeUpdate(chan_id, seq_id, secrets, rev_htlcs,
-            peer_nonce, block_height);
-    }
-
-    ///
-    public override Result!Signature requestSettleSig (/* in */ Hash chan_id,
-        /* in */ uint seq_id) @trusted
-    {
-        return this.flash.requestSettleSig(chan_id, seq_id);
-    }
-
-    ///
-    public override Result!Signature requestUpdateSig (/* in */ Hash chan_id,
-        /* in */ uint seq_id) @trusted
-    {
-        return this.flash.requestUpdateSig(chan_id, seq_id);
-    }
-
-    ///
-    public override void confirmChannelUpdate (/* in */ Hash chan_id,
-        /* in */ uint seq_id) @trusted
-    {
-        this.flash.confirmChannelUpdate(chan_id, seq_id);
-    }
-
-    ///
-    public override Result!Signature requestCloseSig (/* in */ Hash chan_id,
-        /* in */ uint seq_id) @trusted
-    {
-        return this.flash.requestCloseSig(chan_id, seq_id);
-    }
-
-    ///
-    public override void reportPaymentError (/* in */ Hash chan_id,
-        /* in */ OnionError err) @trusted
-    {
-        this.flash.reportPaymentError(chan_id, err);
-    }
+    mixin FlashNodeCommon!();
 }
