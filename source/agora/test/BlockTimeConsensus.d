e7c7daaed6330a105d19adafbde4cb034fa2f956
Andrej Mitrovic <andrej.mitrovich@gmail.com>
2021-04-13 10:17:23 +0900

    Tests: Fix wrong tx check assumptions
    The code was sending tx's to nodes
    and then immediately after queried
    if the nodes accepted those tx's
    by using the hasTransactionHash() API.
    
    However there is a race condition here.
    The nodes will begin a new nomination
    round and may quickly externalize a new
    block, which will remove those txs
    from the tx pool.
    
    Therefore the tests would sometimes
    fail if the nodes externalized the
    blocks faster than the main thread
    called hasTransactionHash().
    
    As an alternative a test-only
    hasAcceptedTxHash() API has been
    added which checks if the given
    tx hash was *ever* accepted into
    the pool, even if it was later
    removed by externalization of
    a new block.
    
    hasTransactionHash() should only
    be used when we know for sure
    a new block should not be
    externalized.

--- source/agora/test/BlockTimeConsensus.d
+++ source/agora/test/BlockTimeConsensus.d
@@ -39,10 +39,10 @@ unittest
     // 8 transactions is enough for 4 blocks with 2 txs each
     txs.each!(tx => nodes[0].putTransaction(tx));
 
-    // wait for propagation
+    // check for propagation
     nodes.each!(node =>
        txs.each!(tx =>
-           node.hasTransactionHash(hashFull(tx)).retryFor(2.seconds)
+           node.hasAcceptedTxHash(hashFull(tx)).retryFor(2.seconds)
     ));
 
     const b0 = nodes[0].getBlocksFrom(0, 1)[0];
