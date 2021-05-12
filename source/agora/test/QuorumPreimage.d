4d7ab584a60534664586742b30944542a3135e20
Andrej Mitrovic <andrej.mitrovich@gmail.com>
2021-05-12 14:24:36 +0900

    QuorumPreimage: Print logs on failure

--- source/agora/test/QuorumPreimage.d
+++ source/agora/test/QuorumPreimage.d
@@ -47,9 +47,7 @@ unittest
     auto network = makeTestNetwork!TestAPIManager(conf);
     network.start();
     scope(exit) network.shutdown();
-    // Disabled, as most of the time they just hide the more useful
-    // quorum prints which is also a `scope (failure)` (see below)
-    // scope(failure) network.printLogs();
+    scope(failure) network.printLogs();
     network.waitForDiscovery();
 
     auto nodes = network.clients;
