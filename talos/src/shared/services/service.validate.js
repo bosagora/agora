46e9661838c693aea226b8dd919e852f5a4c96e3
Geod24 <pro.mathias.lang@gmail.com>
2021-05-17 13:27:09 +0900

    Talos: Update regex to not trigger spurious warning
    Just use the native builtin syntax and remove unnecessary escapes.

--- talos/src/shared/services/service.validate.js
+++ talos/src/shared/services/service.validate.js
@@ -1,29 +1,24 @@
 export const validateDNS = string => {
-  const reqExp = new RegExp("^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z0-9]{2,100}(:[0-9]{1,5})?(\/.*)?$")
-
-  return reqExp.test(string)
+    const reqExp = /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z0-9]{2,100}(:[0-9]{1,5})?(\/.*)?$/;
+    return reqExp.test(string);
 }
 
 export const validateNetwork = string => {
-  const reqExp = new RegExp("^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z0-9]{2,100}(:[0-9]{1,5})?(\/.*)?$")
-
-  return reqExp.test(string)
+    const reqExp = /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z0-9]{2,100}(:[0-9]{1,5})?(\/.*)?$/;
+    return reqExp.test(string);
 }
 
 export const validatSecretKey = string => {
-  const reqExp = new RegExp('^S[0-9a-z]{55}$');
-
-  return reqExp.test(string)
+    const reqExp = /^S[0-9a-z]{55}$/;
+    return reqExp.test(string);
 }
 
 export const validateAddress = string => {
-  const reqExp = new RegExp("^[A-Za-z0-9\.\:\-\@]+$")
-
-  return reqExp.test(string)
+    const reqExp = /^[A-Za-z0-9\.\:\-\@]+$/;
+    return reqExp.test(string);
 }
 
 export const validatePort = string => {
-  const reqExp = new RegExp("^[0-9]+$")
-
-  return reqExp.test(string)
+    const reqExp = /^[0-9]+$/;
+    return reqExp.test(string);
 }
