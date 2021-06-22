03f2200981b73accbf2a4544f27b2718f1126c4f
Geod24 <pro.mathias.lang@gmail.com>
2021-06-22 12:56:31 +0900

    SCP: Add operator() overload for 512 bits hashes
    In order for us to switch from 32 bytes hash to 64 bytes,
    we need to implement this method so that unordered_map
    instantiates correctly.

--- source/scpp/src/util/HashOfHash.h
+++ source/scpp/src/util/HashOfHash.h
@@ -11,4 +11,8 @@ template <> struct hash<stellar::uint256>
 {
     size_t operator()(stellar::uint256 const& x) const noexcept;
 };
+template <> struct hash<stellar::uint512>
+{
+    size_t operator()(stellar::uint512 const& x) const noexcept;
+};
 }
