2900db893d82ccaa9016ad834ffb0fe8052ef1ef
Geod24 <pro.mathias.lang@gmail.com>
2021-07-12 12:04:26 +0900

    Talos: Fix parsing of network address, use it for bind address
    This changes a few things:
    - Add a help text to the 'Network Options' panel, which depends on the selected node type;
    - Fix the IP parsing to actually work: Replace a regexp with a simple function (regexp for IP are hard);
    - The IP parsing is currently limited to IPv4;
    - Instead of configuring the required peers, which isn't used much anymore, configure the interface(s) to listen to;
    - Provide a sensible default.

--- talos/src/services/service.validate.js
+++ talos/src/services/service.validate.js
@@ -3,9 +3,66 @@ export const validateDNS = string => {
     return reqExp.test(string);
 }
 
+export const parseBindAddress = string => {
+    const prefixes = [ "https", "http", "tcp" ];
+
+    // Default values for the scheme and the port
+    // Only the bind address is required to be provided.
+    let result = {
+        // TODO: Change to 'tcp'
+        type: "http",
+        address: string,
+        port: 2826,
+    }
+
+    const defaultError = {
+        error: "Expected an IPv4 bind address with an optional prefix and port, e.g. 'http://0.0.0.0:2826'",
+    };
+
+    if (typeof(string) !== "string")
+        return defaultError;
+
+    for (const element of prefixes) {
+        if (string.startsWith(element + "://"))
+        {
+            result.type = element;
+            result.address = string.slice(element.length + 3);
+            break;
+        }
+    }
+
+    // Parse port
+    const sp = result.address.split(":");
+    if (sp.length > 2)
+        return defaultError;
+    if (sp.length === 2)
+    {
+        const port = parseInt(sp[1]);
+        if (port > 0 && port <= 65535)
+            result.port = port;
+        else
+            return { error: `${sp[1]} is not a valid value for a port (expected 0-65536)` };
+    }
+    result.address = sp[0];
+
+    // Parse IP (it's actually a netmask)
+    const ip = result.address.split('.');
+    if (ip.length !== 4)
+        return defaultError;
+
+    for (const element of ip)
+    {
+        if (element >= 0 && element <= 255)
+            continue;
+        return { error: `${ip[0]} is not a valid IPv4 bind address (value ${element} is out of the 0-255 range)` };
+    }
+
+    return result;
+}
+
 export const validateNetwork = string => {
-    const reqExp = /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([-.]{1}[a-z0-9]+)*\.[a-z0-9]{2,100}(:[0-9]{1,5})?(\/.*)?$/;
-    return reqExp.test(string);
+    const result = parseBindAddress(string);
+    return 'error' in result ? false : true;
 }
 
 export const validateSecretKey = string => {
