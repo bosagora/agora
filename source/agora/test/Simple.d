d114708e7ce58787a4e843db9b79773c0cb23afb
hewison-chris <hewisonchris@gmail.com>
2021-07-14 17:06:05 +0900

    Simple Test: improve reliability of sig catchup
    As we only catchup signatures from up to last payment block then in the
    test we should give time for catchup before creating the blocks in the
    next payment cycle.

--- source/agora/test/Simple.d
+++ source/agora/test/Simple.d
@@ -28,7 +28,10 @@ import agora.test.Base;
 /// Simple test
 unittest
 {
-    auto network = makeTestNetwork!TestAPIManager(TestConf.init);
+    TestConf conf = {
+        payout_period : 10,
+    };
+    auto network = makeTestNetwork!TestAPIManager(conf);
     network.start();
     scope(exit) network.shutdown();
     scope(failure) network.printLogs();
@@ -39,6 +42,12 @@ unittest
 
     // Create blocks for 2 validator cycles and a couple more
     auto target_height = Height(2 * GenesisValidatorCycle + 2);
-    network.generateBlocks(target_height);
-    network.assertSameBlocks(target_height);
+    Height last_height = Height(0);
+    // Use stride of payout period as otherwise sometimes signatures will never catch up
+    iota(Height(conf.payout_period), target_height + 1).stride(Height(conf.payout_period)).each!((Height h)
+    {
+        network.generateBlocks(h);
+        network.assertSameBlocks(h, last_height + 1);
+        last_height = h;
+    });
 }
