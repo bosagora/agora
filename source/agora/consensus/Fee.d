5f4aee807d1a607d2a195bcd0bff4d84caba7f6e
Chris Hewison <hewisonchris@gmail.com>
2021-08-09 17:15:47 +0900

    Include payout for fees in a coinbase transaction
    At the end of a payout period payout fees to the `Commons Budget` and
    the active `validators`.

--- source/agora/consensus/Fee.d
+++ source/agora/consensus/Fee.d
@@ -367,9 +367,6 @@ public class FeeManager
     public void accumulateFees (in Block block, UTXO[] stakes,
         scope UTXOFinder peekUTXO) @trusted
     {
-        if (block.header.height % this.params.PayoutPeriod == 0)
-            this.clearAccumulatedFees();
-
         Amount tot_fee, tot_data_fee;
         this.getTXSetFees(block.txs, peekUTXO, tot_fee, tot_data_fee);
 
@@ -423,12 +420,16 @@ public class FeeManager
     }
 
     /// Clears the accumulated fees
-    public void clearAccumulatedFees () @safe
+    public void clearAccumulatedFees () nothrow @trusted
     {
-        () @trusted {
+        try
+        {
             this.db.execute("DELETE FROM accumulated_fees");
             this.accumulated_fees.clear();
-        } ();
+        } catch (Exception e)
+        {
+            assert(0, e.msg); // Should never happen
+        }
     }
 
     /***************************************************************************
