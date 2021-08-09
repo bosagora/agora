44de52066b808c9119dec54ff69a7fdc07b1e0d4
Geod24 <pro.mathias.lang@gmail.com>
2021-08-09 13:43:51 +0900

    LocalTx test: Use a no-op transaction relayer
    The method being overridden fell out of sync with the parent's pushTransaction,
    for example it wasn't handling acceptable double spend.
    Instead of overriding pushTransaction, a better way to prevent gossipping
    is by providing a no-op transaction relay.

--- source/agora/test/Base.d
+++ source/agora/test/Base.d
@@ -57,6 +57,7 @@ import agora.node.BlockStorage;
 import agora.node.FullNode;
 import agora.consensus.Ledger;
 import agora.node.TransactionPool;
+import agora.node.TransactionRelayer;
 import agora.node.Validator;
 import agora.registry.API;
 import agora.registry.Server;
@@ -2322,4 +2323,17 @@ public class NoPreImageVN : TestValidatorNode
             .filter!(pi => reveal || pi.utxo != self)
             .array();
     }
+}
+
+/// No-op transaction relayed, can be used to prevent gossipping
+public class NoGossipTransactionRelayer : TransactionRelayer
+{
+    ///
+    public override void start () {}
+
+    ///
+    public override void shutdown () @safe {}
+
+    ///
+    public override string addTransaction (in Transaction tx) @safe { return null; }
 }
