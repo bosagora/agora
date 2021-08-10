97f00dc86023424af44c38615b93082508cfb1a4
Geod24 <pro.mathias.lang@gmail.com>
2021-08-10 12:57:17 +0900

    Ensure: Overload toString to correctly format the message

--- source/agora/common/Ensure.d
+++ source/agora/common/Ensure.d
@@ -42,10 +42,60 @@ private final class FormattedException : Exception
         super("An Exception occured");
     }
 
+    /// Overrides `Throwable.message`
     public override const(char)[] message () const return
         @safe pure nothrow @nogc
     {
         return this.buffer[0 .. this.end];
+    }
+
+    /***************************************************************************
+
+        Overrides `Throwable.toString` sink overload
+
+        Unfortunately, `Throwable.toString` does not call `message`,
+        instead calling `msg` directly, which means our overload of `message`
+        does not help. We need to overload this method and replicate the logic
+        in the original code (see `object.d` in druntime for the original).
+
+        Params:
+          sink = The sink to send the piece-meal string to
+
+    ***************************************************************************/
+
+    public override void toString (scope void delegate(in char[]) sink) const scope
+    {
+        import core.internal.string : unsignedToTempString;
+
+        char[20] buffer = void;
+
+        sink(typeid(this).name);
+        sink("@"); sink(file);
+        sink("("); sink(unsignedToTempString(line, buffer)); sink(")");
+
+        if (this.end > 0)
+        {
+            // Aside from a couple stylistic changes,
+            // this is the only line that differs from `Throwable.toString`
+            sink(": "); sink(this.message());
+        }
+        // `Throwable.info` might be `null`, e.g. in a finalizer,
+        // but will probably never be for us.
+        if (this.info)
+        {
+            try
+            {
+                sink("\n----------------");
+                foreach (t; info)
+                {
+                    sink("\n"); sink(t);
+                }
+            }
+            catch (Throwable)
+            {
+                // ignore more errors
+            }
+        }
     }
 }
 
