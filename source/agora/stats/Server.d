7d6020e44411b304e6e3a6eee6e93e44b0f89248
Geod24 <pro.mathias.lang@gmail.com>
2021-08-13 10:25:16 +0900

    Stats server: Set keep alive to 70 seconds
    Workaround issue 2380.

--- source/agora/stats/Server.d
+++ source/agora/stats/Server.d
@@ -19,6 +19,8 @@ import ocean.util.prometheus.collector.CollectorRegistry;
 import vibe.http.server;
 import vibe.http.router;
 
+import core.time;
+
 public class StatsServer
 {
     private HTTPListener http_listener;
@@ -39,6 +41,13 @@ public class StatsServer
 
         auto settings = new HTTPServerSettings;
         settings.port = port;
+        // The following correspond to your scraping interval in Prometheus
+        // The default value for Prometheus is 1 minute:
+        // https://prometheus.io/docs/prometheus/latest/configuration/configuration/
+        // We set it to 70 seconds to avoid a bit of jitter.
+        // See https://github.com/bosagora/agora/issues/2380 and the linked Vibe.d
+        // issue for short-comings of this approach.
+        settings.keepAliveTimeout = 70.seconds;
         this.http_listener = listenHTTP(settings, router);
     }
 
