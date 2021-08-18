ed7ca4bb46333c9100db4ba3b6f6f0ab300f1134
Geod24 <pro.mathias.lang@gmail.com>
2021-08-18 16:26:33 +0900

    Make the name registry a proper DNS server
    The function of the name registry was very similar to that of a DNS server.
    However, it was working at a higher level, which means the clients depended
    on both the DNS server and the registry working.
    Combining both reduces the chances of error.
    
    Additionally, this will allow a few key improvements:
    - Nodes will be able to get an automatic domain name, e.g. `IDENTIFIER.net.bosagora.io`,
      where `IDENTIFIER` is an encoded version of their UTXO hash;
    - This will in turn allow to automatically enable HTTPS for the admin interface,
      allowing a simple way for node operators to connect to their nodes from anywhere;
    - It will integrate with the existing DNS seed, for which we have a client but no server;
    - We get replication for free, as we can setup a slave DNS server which has no knowledge of Agora;

--- source/agora/registry/main.d
+++ source/agora/registry/main.d
@@ -13,11 +13,14 @@
 
 module agora.registry.main;
 
+import agora.common.DNS;
 import agora.registry.API;
 import agora.registry.Server;
+import agora.serialization.Serializer;
 import agora.stats.Server;
 
 import vibe.core.core;
+import vibe.core.net;
 import vibe.http.router;
 import vibe.http.server;
 import vibe.web.rest;
@@ -33,6 +36,7 @@ struct CommandlineArgs
     ushort bind_port = 3003;
     ushort stats_port = 0;
     bool verbose = false;
+    bool nodns = false;
 }
 
 /// Parse the command-line arguments and return a GetoptResult
@@ -49,6 +53,9 @@ public GetoptResult parseCommandLine (ref CommandlineArgs cmdline_args, string[]
         "stats-port",
             "Port where the stats server will bind to (0 to disable), defaults to: " ~ to!string(CommandlineArgs.init.stats_port),
             &cmdline_args.stats_port,
+        "no-dns",
+            "Disable the registry's DNS server",
+            &cmdline_args.nodns,
         "verbose",
             &cmdline_args.verbose,
         );
@@ -82,11 +89,75 @@ private int main (string[] args)
         stats_server = new StatsServer(cmdline_args.stats_port);
 
     auto router = new URLRouter();
-    router.registerRestInterface(new NameRegistry());
+    auto registry = new NameRegistry();
+    router.registerRestInterface(registry);
 
     auto settings = new HTTPServerSettings;
     settings.port = cmdline_args.bind_port;
     settings.bindAddresses = [cmdline_args.bind_address];
     listenHTTP(settings, router);
+
+    if (!cmdline_args.nodns)
+        runTask(() => runDNSServer(registry));
+
     return runEventLoop();
+}
+
+/*******************************************************************************
+
+    Starts the DNS server using the provided registry
+
+    This listens to UDP port 53 for DNS queries, which are then forwarded
+    to the registry to be answered.
+
+    The `canThrow` function is wrapped by a higher level `nothrow` one,
+    which handles the `try` / `catch` in case of fatal error.
+    Throwing from the `canThrow`function is a fatal error,
+    so client connections should not lead to `Exception` escaping this function.
+
+    Params:
+      registry = The name registry to forward the queries to.
+
+*******************************************************************************/
+
+private void runDNSServer_canThrow (NameRegistry registry)
+{
+    // The `listenUDP` needs to be in the `runTask` otherwise we get
+    // a fatal error due to a bug in vibe-core (see comment #2):
+    /// https://github.com/vibe-d/vibe-core/issues/289
+    auto udp = listenUDP(53);
+    // Otherwise `recv` allocates 65k per call (!!!)
+    ubyte[2048] buffer;
+    // `recv` will store the peer address here so we can respond
+    NetworkAddress peer;
+    while (true)
+    {
+        try
+        {
+            auto pack = udp.recv(buffer, &peer);
+            auto query = deserializeFull!Message(pack);
+            auto resp = registry.answerQuestions(query);
+            udp.send(serializeFull(resp), &peer);
+        }
+        catch (Exception exc)
+        {
+            scope (failure) assert(0);
+            stderr.writeln("Exception thrown while handling query: ", exc);
+        }
+    }
+}
+
+/// Ditto
+private void runDNSServer (NameRegistry registry) nothrow
+{
+    try
+        runDNSServer_canThrow(registry);
+    catch (Exception exc)
+    {
+        try
+            stderr.writeln("Fatal error while running the DNS server: ", exc);
+        catch (Exception exc2)
+            printf("Couldn't print message following fatal error in DNS!\n");
+        assert(0);
+    }
 }
