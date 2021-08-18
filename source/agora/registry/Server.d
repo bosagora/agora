0e35f19a0c6e687ad2d55d1880f15cabbcac563e
Geod24 <pro.mathias.lang@gmail.com>
2021-08-18 16:26:33 +0900

    Make the name registry a proper DNS server
    The function of the name registry was very similar to that of a DNS server.
    However, it was working at a higher level, which means the clients depended
    on both the DNS server and the registry working.
    Combining both reduces the chances of error.
    
    Additionally, this will allow a few key improvements:
    - Nodes will be able to get an automatic domain name, e.g. `IDENTIFIER.net.bosagora.io`,
      where `IDENTIFIER` is an encoded version of their UTXO hash;
    - This will in turn allow to automatically enable HTTPS for the admin interface,
      allowing a simple way for node operators to connect to their nodes from anywhere;
    - It will integrate with the existing DNS seed, for which we have a client but no server;
    - We get replication for free, as we can setup a slave DNS server which has no knowledge of Agora;

--- source/agora/registry/Server.d
+++ source/agora/registry/Server.d
@@ -13,14 +13,20 @@
 
 module agora.registry.Server;
 
+import agora.common.DNS;
 import agora.common.Ensure;
 import agora.common.Types;
 import agora.crypto.Hash;
 import agora.crypto.Key;
 import agora.registry.API;
+import agora.serialization.Serializer;
 import agora.stats.Registry;
 import agora.stats.Utils;
 import agora.utils.Log;
+
+import std.algorithm.iteration : splitter;
+import std.algorithm.searching : endsWith;
+import std.socket;
 
 mixin AddLogger!();
 
@@ -97,5 +103,120 @@ public final class NameRegistry: NameRegistryAPI
         this.registry_stats.setMetricTo!"registry_record_count"(registry_map.length);
     }
 
+    /***************************************************************************
+
+        Accepts a DNS message and returns an answer to it.
+
+        The input message should contains a serie of questions,
+        which the server will answer.
+        Currently, only one registry can exists (it assumes authority),
+        and recursion is not yet supported.
+
+        Params:
+          query = The query received by the server
+
+        Returns:
+          An answer that matches the query
+
+    ***************************************************************************/
+
+    public Message answerQuestions (in Message query)
+    {
+        Message reply;
+        reply.header.RA = false; // TODO: Implement
+        reply.header.AA = true;  // TODO: Make configurable
+
+        ResourceRecord answer;
+        answer.type = TYPE.A;
+        answer.class_ = CLASS.IN;
+        answer.ttl = 600;
+
+        foreach (const ref q; query.questions)
+        {
+            auto rcode = this.getValidatorDNSRecord(q, answer);
+            if (rcode != Header.RCode.NoError)
+            {
+                reply.header.RCODE = rcode;
+                break;
+            }
+            reply.answers ~= answer;
+        }
+        return reply.fill(query.header);
+    }
+
+    /***************************************************************************
+
+        Get a single validator's DNS record.
+
+        Queries sent to the server may attempt to look up multiple validators,
+        which is handled by `answerQuestions`. This method looks up a single
+        host name and return all associated addresses.
+
+        Params:
+          question = The question being asked (contains the hostname)
+          answer = A struct to fill with the addresses
+
+        Returns:
+          A code corresponding to the result of the lookup.
+          If the lookup was successful, `Header.RCode.NoError` will be returned.
+          Otherwise, the correct error code (non 0) is returned.
+
+    ***************************************************************************/
+
+    private Header.RCode getValidatorDNSRecord (
+        in Question question, ref ResourceRecord answer)
+    {
+        const public_key = parsePublicKeyFromDomain(question.qname);
+        if (public_key is PublicKey.init)
+            return Header.RCode.FormatError;
+
+        auto payload = public_key in registry_map;
+        // We are authoritative, so we can set `NameError`
+        if (!payload)
+            return Header.RCode.NameError;
+
+        answer.name = question.qname;
+        foreach (idx, const ref addr; (*payload).data.addresses)
+        {
+            uint ip4addr = InternetAddress.parse(addr);
+            if (ip4addr == InternetAddress.ADDR_NONE)
+                continue;
+            answer.rdata ~= serializeFull(ip4addr, CompactMode.No);
+        }
+
+        return Header.RCode.NoError;
+    }
+
+    ///
     mixin DefineCollectorForStats!("registry_stats", "collectRegistryStats");
+}
+
+/// Simplistic parsing function for domain name
+/// This should be made configurable so that the registry can live under different
+/// addresses, e.g. one for the validator and one for flash.
+private PublicKey parsePublicKeyFromDomain (scope const(char)[] domain)
+    @safe nothrow
+{
+    immutable string[2] ends = [
+        ".net.bosagora.io",
+        ".net.bosagora.io.",
+    ];
+
+    // TODO: Improve code and support uppercase queries
+    scope odomain = domain;
+    foreach (e; ends)
+        if (domain.endsWith(e))
+        {
+            domain = domain[0 .. $ - e.length];
+            break;
+        }
+
+    // Not something we know about
+    if (odomain.length == domain.length)
+        return PublicKey.init;
+
+    try
+        return PublicKey.fromString(domain);
+    catch (Exception exc)
+        return PublicKey.init;
 }
