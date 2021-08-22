ce0a9e489c5613d136375c32f4da8c6f988f19fb
hewison-chris <hewisonchris@gmail.com>
2021-08-22 10:13:18 +0900

    testConf: pre-image reveal interval
    In the network tests it would be useful to have the frequency of checks
    to reveal `pre-images` configurable.

--- source/agora/test/Base.d
+++ source/agora/test/Base.d
@@ -1964,6 +1964,9 @@ public struct TestConf
     /// Matches the eponymous field in the `validator` section.
     public Duration preimage_catchup_interval = 100.seconds;
 
+    /// How often the validator should check for preimages to reveal
+    public Duration preimage_reveal_interval = 100.msecs;
+
     /// max failed requests before a node is banned
     /// Matches the eponymous field in the `banman` section.
     public size_t max_failed_requests = 100;
@@ -2127,7 +2130,7 @@ public APIManager makeTestNetwork (APIManager : TestAPIManager = TestAPIManager)
             addresses_to_register : [self_address],
             registry_address : "name.registry",
             recurring_enrollment : test_conf.recurring_enrollment,
-            preimage_reveal_interval : 1.seconds,  // check revealing frequently
+            preimage_reveal_interval : test_conf.preimage_reveal_interval,
             nomination_interval: 100.msecs,
             preimage_catchup_interval: test_conf.preimage_catchup_interval,
             cycle_seed : cycle_seed,
