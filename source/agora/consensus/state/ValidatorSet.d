a80a9ee6c3aee87591c256547a7ee20bc33dad33
Geod24 <pro.mathias.lang@gmail.com>
2021-08-30 14:39:19 +0900

    ValidatorSet: Remove getEnrollmentForKey
    There was a single usage of this function remaining,
    but it was better done by using the already fetched
    ValidatorInfo.

--- source/agora/consensus/state/ValidatorSet.d
+++ source/agora/consensus/state/ValidatorSet.d
@@ -382,36 +382,6 @@ public class ValidatorSet
 
     /***************************************************************************
 
-        Get the utxo_key used for enrollment for the given public key
-
-        Params:
-            height = block height to fetch utxo_key
-            public_key = public key of the validator
-
-        Returns:
-            Return UTXO key as Hash
-
-    ***************************************************************************/
-
-    public Hash getEnrollmentForKey (in Height height, in PublicKey public_key) @trusted nothrow
-    {
-        try
-        {
-            auto results = this.db.execute("SELECT key FROM validator " ~
-                "WHERE public_key = ? AND enrolled_height >= ? AND enrolled_height < ? ",
-                public_key, this.minEnrollmentHeight(height), height);
-            if (!results.empty && results.oneValue!(byte[]).length != 0)
-                return Hash(results.front.peek!(char[])(0));
-        }
-        catch (Exception ex)
-        {
-            log.error("ManagedDatabase operation error: {}", ex.msg);
-        }
-        return Hash.init;
-    }
-
-    /***************************************************************************
-
         Gets the number of active validators at a given block height.
 
         This function finds validators that should be active at a given height,
