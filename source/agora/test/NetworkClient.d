e83e798764700dbd971a580bdc675c96403b129a
Ömer Faruk Irmak <omerfirmak@gmail.com>
2021-09-27 14:47:07 +0300

    Test: address flakey NetworkClient test

--- source/agora/test/NetworkClient.d
+++ source/agora/test/NetworkClient.d
@@ -51,7 +51,7 @@ unittest
     // clear filter after 100 msecs, the above requests will eventually be gossiped
     Thread.sleep(100.msecs);
     nodes[1 .. $].each!(node => node.clearFilter());
-    network.expectHeight(Height(1));
+    network.expectHeightAndPreImg(Height(1));
 }
 
 /// test request timeouts
