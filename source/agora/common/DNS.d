bosagora/agora

commit 1bccc2c2c0a7c1642ef7cdee718728a3471b191e

DNS: Add a factory function for `ResourceRecord`
`ResourceRecord`s are the core of the DNS system, and are essentially
an `enum` type when framed from a D point of view.
However, they are currently defined following the wire format,
which is less than an ideal API for the user.
As a step towards having a better API for them, a factory function needs
to be introduced, hiding the underlying details / boilerplate.

source/agora/common/DNS.d
@@ -588,6 +588,34 @@ public struct Question
 /// https://datatracker.ietf.org/doc/html/rfc1035#section-4.1.3
 public struct ResourceRecord
 {
+    /// Make a record of the given type
+    public static ResourceRecord make (TYPE type) (Domain name, uint ttl, ubyte[] rdata)
+    {
+        return ResourceRecord(name, type, CLASS.IN, ttl, data);
+    }
+
+    /// Make a record of SOA type
+    public static ResourceRecord make (TYPE type : TYPE.SOA) (
+        Domain name, uint ttl, in SOA soa)
+    {
+        return ResourceRecord(name, TYPE.SOA, CLASS.IN, ttl, soa.serializeFull(CompactMode.No));
+    }
+
+    /// Make a record of CNAME type
+    public static ResourceRecord make (TYPE type : TYPE.CNAME) (
+        Domain name, uint ttl, in Domain cname)
+    {
+        return ResourceRecord(name, TYPE.CNAME, CLASS.IN, ttl, cname.serializeFull());
+    }
+
+    /// Make a record of A type
+    public static ResourceRecord make (TYPE type : TYPE.A) (
+        Domain name, uint ttl, uint[] ipv4...) @trusted
+    {
+        auto rdata = (cast(ubyte*) ipv4.ptr)[0 .. uint.sizeof * ipv4.length].dup;
+        return ResourceRecord(name, TYPE.CNAME, CLASS.IN, ttl, rdata);
+    }
+
     /// A domain name to which this resource record pertains.
     public Domain name;
 

source/agora/node/Registry.d
@@ -551,37 +551,35 @@ private struct TypedPayload
     public ResourceRecord toRR (const char[] qname) const scope
         @safe
     {
-        ResourceRecord answer;
-        answer.class_ = CLASS.IN; // Validated by the caller
-        answer.type = this.type;
-        answer.name = qname;
-
-        if (this.type == TYPE.CNAME)
+        Domain name = Domain(qname);
+        switch (this.type)
         {
+        case TYPE.CNAME:
+            assert(this.payload.data.addresses.length == 1);
+            // FIXME: Use a proper TTL
+
             /* If it's a CNAME, it has to be to another domain, as we don't
              * yet support aliases in the same zone, hence the algorithm in
              * "RFC1034: 4.3.2. Algorithm" can be reduced to "return the CNAME".
              */
             assert(this.payload.data.addresses.length == 1);
-            answer.rdata = Domain(this.payload.data.addresses[0].host).serializeFull();
-            // We don't provide recursion yet, so just return this
-            // and let the caller figure it out.
-        }
-        else if (this.type == TYPE.A)
-        {
+            return ResourceRecord.make!(TYPE.CNAME)(name, 0, Domain(this.payload.data.addresses[0].host));
+
+        case TYPE.A:
+            // FIXME: Remove allocation
+            scope uint[] tmp = new uint[](this.payload.data.addresses.length);
             foreach (idx, addr; this.payload.data.addresses)
             {
-                uint ip4addr = InternetAddress.parse(addr.host);
-                ensure(ip4addr != InternetAddress.ADDR_NONE,
+                tmp[idx] = InternetAddress.parse(addr.host);
+                ensure(tmp[idx] != InternetAddress.ADDR_NONE,
                        "DNS: Address '{}' (index: {}) is not an A record (record: {})",
                        addr, idx, this);
-                answer.rdata ~= serializeFull(ip4addr, CompactMode.No);
             }
-        }
-        else
+            return ResourceRecord.make!(TYPE.A)(name, 0, tmp);
+        default:
             ensure(0, "Unknown type: {} - {}", this.type, this);
-
-        return answer;
+            assert(0);
+        }
     }
 }
 
@@ -751,9 +749,9 @@ private struct ZoneData
         else if (q.qtype == QTYPE.SOA)
         {
             if (matches)
-                reply.answers ~= this.toRR();
+                reply.answers ~= ResourceRecord.make!(TYPE.SOA)(this.root, 0, this.soa);
             else
-                reply.authorities ~= this.toRR();
+                reply.authorities ~= ResourceRecord.make!(TYPE.SOA)(this.root, 0, this.soa);
 
             return Header.RCode.NoError;
         }
@@ -793,7 +791,7 @@ private struct ZoneData
     {
         log.info("Performing AXFR for {} ({} entries)", this.root.value, this.count());
 
-        auto soa = this.toRR();
+        auto soa = ResourceRecord.make!(TYPE.SOA)(this.root, 0, this.soa);
         reply.answers ~= soa;
 
         foreach (const ref payload; this)
@@ -842,7 +840,7 @@ private struct ZoneData
             return Header.RCode.NameError;
 
         reply.answers ~= payload.toRR(question.qname);
-        reply.authorities ~= this.toRR(); // optional
+        reply.authorities ~= ResourceRecord.make!(TYPE.SOA)(this.root, 0, this.soa); // optional
         return Header.RCode.NoError;
     }
 
@@ -967,23 +965,6 @@ private struct ZoneData
         }
 
         this.soa.serial = cast(uint) Clock.currTime(UTC()).toUnixTime();
-    }
-
-    /***************************************************************************
-
-         Returns:
-           A ResourceRecord matching this zone's SOA.
-
-    ***************************************************************************/
-
-    public ResourceRecord toRR () const @safe
-    {
-        ResourceRecord result;
-        result.name = this.soa.mname;
-        result.type = TYPE.SOA;
-        result.class_ = CLASS.IN;
-        result.rdata = this.soa.serializeFull(CompactMode.No);
-        return result;
     }
 
     /***************************************************************************

commit 8bbf141790c50185be9b49b437edcc8b33c9a5ac

DNS: Fix invalid TYPE for A resource record factory
Commit 1bccc2c2c0a7c1642ef7cdee718728a3471b191e caused A records to
appear as CNAME records in query responses.

source/agora/common/DNS.d
@@ -611,8 +611,9 @@ public struct ResourceRecord
     public static ResourceRecord make (TYPE type : TYPE.A) (
         Domain name, uint ttl, uint[] ipv4...) @trusted
     {
+
         auto rdata = (cast(ubyte*) ipv4.ptr)[0 .. uint.sizeof * ipv4.length].dup;
-        return ResourceRecord(name, TYPE.CNAME, CLASS.IN, ttl, rdata);
+        return ResourceRecord(name, TYPE.A, CLASS.IN, ttl, rdata);
     }
 
     /// A domain name to which this resource record pertains.