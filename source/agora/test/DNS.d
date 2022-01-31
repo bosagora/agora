1bb1b4653c98373d26a09817e9859d366fa35cd7
Muhammed Kadir Yücel <muhammed.yucel@bosagora.io>
2022-01-31 11:05:08 +0300

    Test: Add registry DNS record tests
    SOA, NS, CNAME and A record tests are added

--- source/agora/test/DNS.d
+++ source/agora/test/DNS.d
@@ -13,7 +13,9 @@ module agora.test.DNS;
 
 version (unittest):
 
+import agora.common.DNS;
 import agora.test.Base;
+import agora.utils.WellKnownKeys;
 import geod24.LocalRest;
 import geod24.concurrency;
 
@@ -27,11 +29,46 @@ unittest
     scope(failure) network.printLogs();
     network.waitForDiscovery();
 
+    const node_addr = NODE2.address.toString;
+
     FiberScheduler scheduler = new FiberScheduler;
     scheduler.start(
         () {
-            auto addr = network.dns_resolver.resolve(Address("agora://boa1xzval2a3cdxv28n6slr62wlczslk3juvk7cu05qt3z55ty2rlfqfc6egsh2.validators.unittest.bosagora.io"));
+            auto addr = network.dns_resolver.resolve(
+                Address("agora://" ~ node_addr ~ ".validators.unittest.bosagora.io"));
             assert(addr.host.startsWith("10.0.0."));
+
+            Message query;
+
+            // Query for SOA record
+            auto name = Domain.fromSafeString("validators.unittest.bosagora.io.");
+            query.questions ~= Question(name, QTYPE.SOA, QCLASS.IN);
+            query.fill(query.header);
+            auto result = network.dns_resolver.query(query);
+            assert(result.length == 1);
+            assert(result[0].name == name);
+            assert(result[0].type == TYPE.SOA);
+            assert(result[0].rdata.soa.mname == Domain.fromSafeString("name.registry."));
+            assert(result[0].rdata.soa.rname == Domain.fromSafeString("test.testnet."));
+
+            // Query for NS record
+            query.questions[0].qtype = QTYPE.NS;
+            result = network.dns_resolver.query(query);
+            assert(result.length == 1);
+            assert(result[0].type == TYPE.NS);
+            assert(result[0].rdata.name == Domain.fromSafeString("name.registry."));
+
+            // A record
+            auto node_name = Domain.fromString(node_addr ~ ".validators.unittest.bosagora.io.");
+            query.questions[0] = Question(node_name, QTYPE.A, QCLASS.IN);
+            result = network.dns_resolver.query(query);
+            assert(result.length == 1);
+            assert(result[0].type == TYPE.A);
+
+            // CNAME record
+            query.questions[0].qtype = QTYPE.CNAME;
+            result = network.dns_resolver.query(query);
+            assert(result.length == 0);
         }
     );
 }
