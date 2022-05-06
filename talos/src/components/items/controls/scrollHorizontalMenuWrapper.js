72b5848d74964569ea433562b7feecf1983d1a3c
Muhammed Kadir Yücel <muhammed.yucel@bosagora.io>
2022-05-06 15:45:47 +0300

    Update Talos dependencies

--- talos/src/components/items/controls/scrollHorizontalMenuWrapper.js
+++ talos/src/components/items/controls/scrollHorizontalMenuWrapper.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Scrollbars } from 'react-custom-scrollbars';
+import { Scrollbars } from 'react-custom-scrollbars-2';
 import { SpringSystem } from 'rebound';
 
 import styles from "./scrollHorizontalMenuWrapper.module.scss"
